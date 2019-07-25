8dafbcaf96415b624be35bbade9403ea88c079ee
Alec <ibealec@gmail.com>
2019-07-24 17:03:40 -0700

    Added homework instructions to unit 19 and 20

---
+++ 01-Class-Content/20-state/02-Homework/Solution/client/src/components/Search/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "../Grid";
+import SearchForm from "../SearchForm";
+
+function Search() {
+  return (
+      <div>
+        <Container >
+          <p className="text-center"></p>
+          <SearchForm />
+        </Container>
+      </div>
+  );
+}
+
+export default Search;
