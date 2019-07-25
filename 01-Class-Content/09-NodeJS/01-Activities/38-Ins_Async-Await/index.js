d6e79785b0119a55729649a24d7d52773abca2b0
ceckenrode <chriseckenrode@me.com>
2019-07-25 09:19:21 -0400

    add activity 39

---
+++ 01-Class-Content/09-NodeJS/01-Activities/38-Ins_Async-Await/index.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+getMovie();
+
+async function getMovie() {
+  const { movie } = await inquirer.prompt({
+    message: "Search a movie:",
+    name: "movie"
+  });
+
+  const { data } = await axios.get(`https://www.omdbapi.com/?t=${movie}&apikey=trilogy`);
+
+  console.log(data);
+}
