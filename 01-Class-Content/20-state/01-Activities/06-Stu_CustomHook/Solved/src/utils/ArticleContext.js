fa84ad2a21798eeb8a04251f24d7a8ac7c697b26
Tammer Galal <tammergalal20@gmail.com>
2019-08-06 14:46:29 -0400

    fixing dupe folders

--- 01-Class-Content/20-state/01-Activities/06-Stu_CustomHook/Solved/src/utils/ArticleContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ArticleContext = React.createContext({
-  title: "",
-  description: "",
-  url: ""
-});
-
-export default ArticleContext;
--- 01-Class-Content/20-state/01-Activities/08-Stu_PropDrilling/Unsolved/src/components/CardTitleText/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CardTitleText() {
-  return <h2>Name Here</h2>;
-}
-
-export default CardTitleText;
