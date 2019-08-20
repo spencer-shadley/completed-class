1bc7b2e8e8a8b1fd8ef4c9c1b19e771618274c94
ceckenrode <chriseckenrode@me.com>
2019-08-20 16:24:01 -0400

    More LP updates

--- 01-Class-Content/09-NodeJS/01-Activities/34-Ins_Introduce-Promises/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/34-Ins_Introduce-Promises/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const util = require("util");
 
-// The built-in util package can be used to 
+  // The built-in util package can be used to create Promise-based versions of functions using node style callbacks
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
