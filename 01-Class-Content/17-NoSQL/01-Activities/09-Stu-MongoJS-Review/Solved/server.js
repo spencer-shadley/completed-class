88e00bdb967c856074b65c744c5011255afb955b
Tammer Galal <tammergalal20@gmail.com>
2019-08-20 13:05:02 -0400

    chunking variable declarations

--- 01-Class-Content/17-NoSQL/01-Activities/09-Stu-MongoJS-Review/Solved/server.js
+++ 01-Class-Content/17-NoSQL/01-Activities/09-Stu-MongoJS-Review/Solved/server.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const mongojs = require("mongojs");
-
 const logger = require("morgan");
+
+const databaseUrl = "warmup";
+const collections = ["books"];
+const db = mongojs(databaseUrl, collections);
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-const databaseUrl = "warmup";
-const collections = ["books"];
 
-const db = mongojs(databaseUrl, collections);
+
 db.on("error", error => {
   console.log("Database Error:", error);
 });
