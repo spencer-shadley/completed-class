b8bce98b76f7986041ad9f3f0868dfc7a185dcd3
Tammer Galal <tammergalal20@gmail.com>
2019-08-22 15:53:17 -0400

    adding instructor demo for prototypes

---
+++ 01-Class-Content/10-OOP/01-Activities/05-Ins_Object-Prototypes/prototype-demo.js
@@ -0,0 +1,18 @@
+myArray = [2, 4, 6, 8];
+console.log(myArray);
+
+myArray.forEach((num) => console.log(num));
+
+myArray.map((x) => console.log(x * 2));
+
+myObject = {
+    name: "Eric",
+    age: 28,
+    occupation: "Curriculum Dev"
+};
+
+console.log(myObject.hasOwnProperty('name'));
+
+console.log("Hello".toLowerCase());
+
+console.log((1337).toString());
