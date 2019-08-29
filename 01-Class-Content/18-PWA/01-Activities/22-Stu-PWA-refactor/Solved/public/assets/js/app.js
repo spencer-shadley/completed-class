83fecc6cabad01934d0f68e143d9331a5daa4b97
Alec Branaa <alec@ossenx.com>
2019-08-28 23:48:46 -0700

    Renumbered 18.2 and 18.3 activities

--- 01-Class-Content/18-PWA/01-Activities/22-Stu-PWA-refactor/Solved/public/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-import { createCards } from "./cardCreation";
-
-export function loadImages() {
-  fetch("/api/images").then((res) => res.json())
-    .then((data) => createCards(data));
-}
-
-loadImages();
