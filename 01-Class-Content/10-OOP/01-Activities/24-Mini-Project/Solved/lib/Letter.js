0cba65adc3be0f6623122e464dfab695b15a8a6c
Tammer Galal <tammergalal20@gmail.com>
2019-11-15 10:36:20 -0500

    fixing mini project

--- 01-Class-Content/10-OOP/01-Activities/24-Mini-Project/Solved/lib/Letter.js
+++ 01-Class-Content/10-OOP/01-Activities/24-Mini-Project/Solved/lib/Letter.js
@@ -6,6 +6,31 @@ class Letter {
     this.visible = !/[a-z1-9]/i.test(char);
     this.char = char;
   }
+
+  // Returns either an underscore or the underlying character depending on `this.visible`
+  //    because we call this function toString, when we call `this.letters.join` in
+  //    Word.js, JavaScript automatically uses the value we return here
+  toString() {
+    if (this.visible === true) {
+      return this.char;
+    }
+    return "_";
+  }
+
+  getSolution() {
+    return this.char;
+  }
+
+  // Accepts a user's guess
+  guess(charGuess) {
+    if (charGuess.toUpperCase() === this.char.toUpperCase()) {
+      this.visible = true;
+      return true;
+    }
+
+    // Otherwise return false
+    return false;
+  }
 }
 
 module.exports = Letter;
--- 01-Class-Content/10-OOP/01-Activities/24-Mini-Project/Solved/lib/Word.js
+++ 01-Class-Content/10-OOP/01-Activities/24-Mini-Project/Solved/lib/Word.js
@@ -10,6 +10,44 @@ class Word {
       return new Letter(char);
     });
   }
+
+  getSolution() {
+    return this.letters
+      .map(function(letter) {
+        // iterate over each letter
+        return letter.getSolution(); // return the solution for each to form an array of solved letters
+      })
+      .join(""); // create a string from the array of solved letters
+  }
+
+  // setting `toString()` as a method lets us concatenate it like we would a string!
+  toString() {
+    return this.letters.join(" "); // see Letter.prototype.toString in Letter.js
+  }
+
+  guessLetter(char) {
+    // Checks to see if any of the letters in the array match the user's guess and updates `foundLetter`
+    let foundLetter = false;
+    this.letters.forEach(function(letter) {
+      if (letter.guess(char)) {
+        foundLetter = true;
+      }
+    });
+
+    // Print the word guessed so far--because we set the method for toString,
+    //  JavaScript will automatically concatenate this even if we don't call toString
+    console.log("\n" + this + "\n");
+    // return whether we found a letter
+    return foundLetter;
+  }
+
+  // Returns true if all letters in the word have been guessed
+  guessedCorrectly() {
+    // The `every` method returns true if the callback function returns true for every element in the array
+    return this.letters.every(function(letter) {
+      return letter.visible;
+    });
+  }
 }
 
 module.exports = Word;
