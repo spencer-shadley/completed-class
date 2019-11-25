be28100470e15991b27edf13705dab433e800583
Tammer Galal <tammergalal20@gmail.com>
2019-11-25 12:05:31 -0500

    reworking provided code

---
+++ 01-Class-Content/03-JavaScript/02-Homework/Develop/script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  copyBtn.removeAttribute("disabled");
+  copyBtn.focus();
+}
+
+function copyToClipboard() {
+  // BONUS 
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// BONUS EVENT LISTENER
