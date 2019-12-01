ab7ba0db1d7e314cc554290519fed866e0eaa976
Spencer Shadley <spencershadley@utexas.edu>
2019-11-30 22:57:18 -0800

    updated solutions

--- 01-Class-Content/04-Web-APIs/01-Activities/09-Stu_SpeedReader/Solved/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/09-Stu_SpeedReader/Solved/script.js
@@ -1,24 +1,24 @@
-var poem = "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
-var words = poem.split(" ");
+var poem = 'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
+var words = poem.split(' ');
 
-var mainEl = document.getElementById("main");
-var readEl = document.getElementById("read");
-var timerEl = document.getElementById("countdown");
-var bodyEl = document.createElement("div");
+var mainEl = document.getElementById('main');
+var readEl = document.getElementById('read');
+var timerEl = document.getElementById('countdown');
+var bodyEl = document.createElement('div');
 
 var i = 0;
 
-var wordsPerMillisecond = prompt("How many words per millisecond would you like to read?");
+var wordsPerMillisecond = prompt('How many words per millisecond would you like to read?');
 
 function prepareRead() {
   var timeLeft = 5;
 
-  var timeInterval = setInterval(function() {
-    timerEl.textContent = timeLeft + " seconds remaining";
-    timeLeft--;
+  var timeInterval = setInterval(function () {
+    timerEl.textContent = timeLeft + ' seconds remaining';
+    --timeLeft;
 
     if (timeLeft === 0) {
-      timerEl.textContent = "";
+      timerEl.textContent = '';
       speedRead();
       clearInterval(timeInterval);
     }
@@ -26,21 +26,18 @@ function prepareRead() {
   }, 1000);
 }
 
-
 function speedRead() {
   mainEl.append(bodyEl);
 
-  var poemInterval = setInterval(function() {
+  var poemInterval = setInterval(function () {
     if (words[i] === undefined) {
       clearInterval(poemInterval);
     } else {
       mainEl.textContent = words[i];
-      i++;
+      ++i;
     }
 
   }, wordsPerMillisecond);
 }
 
 prepareRead();
-
-
