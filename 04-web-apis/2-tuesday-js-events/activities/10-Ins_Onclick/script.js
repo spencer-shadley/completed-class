fa9c74b7c826bdae5fd260567f884f864c36e21f
Spencer Shadley <spencershadley@utexas.edu>
2019-12-01 17:40:53 -0800

    style updates

--- 04-web-apis/2-tuesday-js-events/activities/10-Ins_Onclick/script.js
+++ 04-web-apis/2-tuesday-js-events/activities/10-Ins_Onclick/script.js
@@ -1,14 +1,13 @@
-var themeSwitcher = document.querySelector("#theme-switcher");
-var container = document.querySelector(".container");
-var mode = "dark";
+var themeSwitcher = document.querySelector('#theme-switcher');
+var container = document.querySelector('.container');
+var mode = 'dark';
 
-themeSwitcher.addEventListener("click", function() {
-  if (mode === "dark") {
-    mode = "light";
-    container.setAttribute("class", "light");
+themeSwitcher.addEventListener('click', function () {
+  if (mode === 'dark') {
+    mode = 'light';
   }
   else {
-    mode = "dark";
-    container.setAttribute("class", "dark");
+    mode = 'dark';
   }
+  container.setAttribute('class', mode);
 });
--- 04-web-apis/2-tuesday-js-events/preclass-drill/drill.md
+++ 04-web-apis/2-tuesday-js-events/preclass-drill/drill.md
@@ -1,4 +1,4 @@
-Write a function that takes in an integer and outputs the sum of all the numbers from 1 to that integer.
+# Write a function that takes in an integer and outputs the sum of all the numbers from 1 to that integer.
 
 Ex:
 Input: 2
@@ -9,3 +9,5 @@ Output: 10  (because 1 + 2 + 3 + 4 = 10)
 
 Input: 10
 Output: 55
+
+## Bonus - Print a message every time the accrued sum is divisible by 10
