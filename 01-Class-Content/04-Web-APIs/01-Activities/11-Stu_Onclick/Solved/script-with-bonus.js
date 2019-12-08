cae4e15d5c44df8088a5baf0364753712a59bf46
Spencer Shadley <spencershadley@utexas.edu>
2019-12-08 13:34:02 -0800

    added bonuses

--- 01-Class-Content/04-Web-APIs/01-Activities/11-Stu_Onclick/Solved/script-with-bonus.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/11-Stu_Onclick/Solved/script-with-bonus.js
@@ -1,4 +1,5 @@
 var count = 0;
+var updateAmountElement = document.querySelector('#update-amount');
 var incrementElement = document.querySelector('#increment');
 var decrementElement = document.querySelector('#decrement');
 var countElement = document.querySelector('#count');
@@ -14,10 +15,26 @@ function updateCount(updatedCount) {
   }
 }
 
+function getUpdatemount() {
+  var amount = parseInt(updateAmountElement.value);
+  if (isNaN(amount)) {
+    return 5; // default to 5
+  }
+  return amount;
+}
+
 incrementElement.addEventListener('click', function () {
-  updateCount(count + 1);
+  updateCount(count + getUpdatemount());
 });
 
 decrementElement.addEventListener('click', function () {
+  updateCount(count - getUpdatemount());
+});
+
+incrementElement.addEventListener('mouseover', function () {
+  updateCount(count + 1);
+});
+
+decrementElement.addEventListener('mouseout', function () {
   updateCount(count - 1);
 });
