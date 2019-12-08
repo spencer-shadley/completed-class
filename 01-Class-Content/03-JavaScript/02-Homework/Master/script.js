7cd8b1c1b6c17dc01138fd90b03ea254e464afba
Spencer Shadley <spencershadley@utexas.edu>
2019-12-07 23:20:53 -0800

    massive refactor

--- 01-Class-Content/03-JavaScript/02-Homework/Master/script.js
+++ 01-Class-Content/03-JavaScript/02-Homework/Master/script.js
@@ -1,146 +1,82 @@
-// Array of special characters to be included in password
-var specialCharacters = [
-  "@",
-  "%",
-  "+",
-  "\\",
-  "/",
-  "'",
-  "!",
-  "#",
-  "$",
-  "^",
-  "?",
-  ":",
-  ",",
-  ")",
-  "(",
-  "}",
-  "{",
-  "]",
-  "[",
-  "~",
-  "-",
-  "_",
-  "."
-];
+// each set of characters leveraging ASCII sequences
+// https://www.computerhope.com/jargon/a/ascii.htm
+// https://www.webopedia.com/TERM/A/ASCII.html
+// alternatively you could create the arrays "by hand"
+// var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// var lowerCasedCharacters = ['a', 'b', ...];
+var numericCharacters = createArrayInRange('0', '9');
+var lowerCasedCharacters = createArrayInRange('a', 'z');
+var upperCasedCharacters = createArrayInRange('A', 'Z');
+var specialCharactersFromBang = createArrayInRange('!', '/');
+var specialCharactersFromColon = createArrayInRange(':', '@');
+var specialCharactersFromBracket = createArrayInRange('[', '`');
+var specialCharacters = specialCharactersFromBang
+  .concat(specialCharactersFromColon)
+  .concat(specialCharactersFromBracket);
 
-// Array of numeric characters to be included in password
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-];
+// create an array including characters beginning at the
+// ASCII value of start up to, including, the ASCII value for end
+function createArrayInRange(start, end) {
+  var arr = [];
+  for (var i = start.charCodeAt(0); i <= end.charCodeAt(0); ++i) {
+    arr.push(String.fromCharCode(i));
+  }
+  return arr;
+}
 
 // Function to prompt user for password options
 function getPasswordOptions() {
   // Variable to store length of password from user input
   var length = parseInt(prompt(
-    "How many characters would you like your password to contain?"
+    'How many characters would you like your password to contain?'
   ));
 
   // Conditional statement to check if password length is a number. Prompts end if this evaluates false
   if (isNaN(length) === true) {
-    alert("Password length must be provided as a number");
+    alert('Password length must be provided as a number');
     return;
   }
 
   // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
   if (length < 8) {
-    alert("Password length must be at least 8 characters");
+    alert('Password length must be at least 8 characters');
     return;
   }
 
   // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
   if (length > 128) {
-    alert("Password length must less than 129 characters");
+    alert('Password length must be less than 129 characters');
     return;
   }
 
   // Variable to store boolean regarding the inclusion of special characters
   var hasSpecialCharacters = confirm(
-    "Click OK to confirm including special characters."
+    'Click OK to confirm including special characters.'
   );
 
   // Variable to store boolean regarding the inclusion of numeric characters
   var hasNumericCharacters = confirm(
-    "Click OK to confirm including numeric characters."
+    'Click OK to confirm including numeric characters.'
   );
 
   // Variable to store boolean regarding the inclusion of lowercase characters
   var hasLowerCasedCharacters = confirm(
-    "Click OK to confirm including lowercase characters."
+    'Click OK to confirm including lowercase characters.'
   );
 
   // Variable to store boolean regarding the inclusion of uppercase characters
   var hasUpperCasedCharacters = confirm(
-    "Click OK to confirm including uppercase characters."
+    'Click OK to confirm including uppercase characters.'
   );
 
   // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
   if (
-    hasSpecialCharacters === false &&
-    hasNumericCharacters === false &&
-    hasLowerCasedCharacters === false &&
-    hasUpperCasedCharacters === false
+    !hasSpecialCharacters &&
+    !hasNumericCharacters &&
+    !hasLowerCasedCharacters &&
+    !hasUpperCasedCharacters
   ) {
-    alert("Must select at least one character type");
+    alert('Must select at least one character type');
     return;
   }
 
@@ -217,37 +153,37 @@ function generatePassword() {
   }
 
   // Transform the result into a string and pass into writePassword
-  return result.join("");
+  return result.join('');
 }
 
 // Get references to the #copy and #generate elements
-var copyBtn = document.querySelector("#copy");
-var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector('#copy');
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
+  copyBtn.removeAttribute('disabled');
   copyBtn.focus();
 }
 
 function copyToClipboard() {
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.select();
-  document.execCommand("copy");
+  document.execCommand('copy');
 
   alert(
-    "Your password " + passwordText.value + " was copied to your clipboard."
+    'Your password ' + passwordText.value + ' was copied to your clipboard.'
   );
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
 
 // Add event listener to copy button
-copyBtn.addEventListener("click", copyToClipboard);
+copyBtn.addEventListener('click', copyToClipboard);
