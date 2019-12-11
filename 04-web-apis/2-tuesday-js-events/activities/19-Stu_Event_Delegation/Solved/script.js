6e4fd44b92b271cceb73ca54a880a6a4611066a3
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2019-12-10 21:30:56 -0800

    Stu_Event_Delegation Solved

---
+++ 04-web-apis/2-tuesday-js-events/activities/19-Stu_Event_Delegation/Solved/script.js
@@ -0,0 +1,53 @@
+
+var addButton = document.querySelector('#add-btn');
+var peopleListElement = document.querySelector('#people-list');
+var nameElement = document.querySelector('#name');
+var modalElement = document.querySelector('#modal-container');
+var modalNameElement = document.querySelector('#modal-name');
+var descriptionElement = document.querySelector('#description');
+var closeElement = document.querySelector('.close');
+var saveButton = document.querySelector('#save');
+
+var people = [{ name: 'Spencer' }];
+var currentId = 0;
+
+function addPersonToList(event) {
+  event.preventDefault();
+  var name = nameElement.value;
+  var li = document.createElement('li');
+  li.id = people.length;
+  li.innerHTML = name + ' <button>edit</button>';
+  people.push({ name: name });
+  peopleListElement.append(li);
+}
+
+function close() {
+  modalElement.style.display = 'none';
+}
+
+function handleClick(event) {
+  if (event.target.matches('button')) {
+    event.preventDefault();
+    modalElement.style.display = 'block';
+    currentId = parseInt(event.target.parentElement.id);
+    var name = people[currentId].name;
+    var description = people[currentId].description;
+    modalNameElement.textContent = name;
+    descriptionElement.value = description ? description : '';
+  }
+}
+
+closeElement.addEventListener('click', close);
+saveButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  people[currentId].description = descriptionElement.value;
+  close();
+});
+
+addButton.addEventListener('click', addPersonToList);
+peopleListElement.addEventListener('click', handleClick);
+document.addEventListener('click', function (event) {
+  if (event.target === modalElement) {
+    close();
+  }
+});
