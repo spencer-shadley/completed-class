c1a3867540c4ec102fa7530a9213e69e23be3bb2
Spencer Shadley <spencershadley@utexas.edu>
2019-12-12 19:15:20 -0800

    single quote

--- 01-Class-Content/04-Web-APIs/01-Activities/24-Ins_Data-Attributes/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/24-Ins_Data-Attributes/script.js
@@ -1,17 +1,17 @@
-var imageContainer = document.querySelector(".img-container");
+var imageContainer = document.querySelector('.img-container');
 
-imageContainer.addEventListener("click", function(event) {
+imageContainer.addEventListener('click', function(event) {
   var element = event.target;
 
-  if (element.matches("img")) {
-    var state = element.getAttribute("data-state");
+  if (element.matches('img')) {
+    var state = element.getAttribute('data-state');
 
-    if (state === "still") {
-      element.setAttribute("data-state", "animate");
-      element.setAttribute("src", element.getAttribute("data-animate"));
-    } else if (state === "animate") {
-      element.setAttribute("data-state", "still");
-      element.setAttribute("src", element.getAttribute("data-still"));
+    if (state === 'still') {
+      element.setAttribute('data-state', 'animate');
+      element.setAttribute('src', element.getAttribute('data-animate'));
+    } else if (state === 'animate') {
+      element.setAttribute('data-state', 'still');
+      element.setAttribute('src', element.getAttribute('data-still'));
     }
   }
 });
