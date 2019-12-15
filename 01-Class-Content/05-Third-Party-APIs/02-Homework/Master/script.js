8a3430ce9cf1621d7976db8433224151f3897fce
Spencer Shadley <spencershadley@utexas.edu>
2019-12-14 20:14:00 -0800

    updated strings

--- 01-Class-Content/05-Third-Party-APIs/02-Homework/Master/script.js
+++ 01-Class-Content/05-Third-Party-APIs/02-Homework/Master/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   // listen for save button clicks
-  $(".saveBtn").on("click", function() {
+  $('.saveBtn').on('click', function() {
     // get nearby values
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+    var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
 
     // save in localStorage
     localStorage.setItem(time, value);
@@ -14,21 +14,21 @@ $(document).ready(function() {
     var currentHour = moment().hours();
 
     // loop over time blocks
-    $(".time-block").each(function() {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    $('.time-block').each(function() {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
 
       // check if we've moved past this time
       if (blockHour < currentHour) {
-        $(this).addClass("past");
+        $(this).addClass('past');
       } 
       else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
+        $(this).removeClass('past');
+        $(this).addClass('present');
       } 
       else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
       }
     });
   }
@@ -39,16 +39,16 @@ $(document).ready(function() {
   var interval = setInterval(hourUpdater, 15000);
 
   // load any saved data from localStorage
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
   // display current day on page
-  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  $('#currentDay').text(moment().format('dddd, MMMM Do'));
 });
