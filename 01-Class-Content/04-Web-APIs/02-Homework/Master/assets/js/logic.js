bd20ccec932e87922f4ee0bdd8b2ed0dddd93d0e
Spencer Shadley <spencershadley@utexas.edu>
2019-12-20 19:50:36 -0800

    fixed style of hw

--- 01-Class-Content/04-Web-APIs/02-Homework/Master/assets/js/logic.js
+++ 01-Class-Content/04-Web-APIs/02-Homework/Master/assets/js/logic.js
@@ -4,25 +4,25 @@ var time = questions.length * 15;
 var timerId;
 
 // variables to reference DOM elements
-var questionsEl = document.getElementById("questions");
-var timerEl = document.getElementById("time");
-var choicesEl = document.getElementById("choices");
-var submitBtn = document.getElementById("submit");
-var startBtn = document.getElementById("start");
-var initialsEl = document.getElementById("initials");
-var feedbackEl = document.getElementById("feedback");
+var questionsEl = document.getElementById('questions');
+var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
+var feedbackEl = document.getElementById('feedback');
 
 // sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+var sfxRight = new Audio('assets/sfx/correct.wav');
+var sfxWrong = new Audio('assets/sfx/incorrect.wav');
 
 function startQuiz() {
   // hide start screen
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "hide");
+  var startScreenEl = document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'hide');
 
   // un-hide questions section
-  questionsEl.removeAttribute("class");
+  questionsEl.removeAttribute('class');
 
   // start timer
   timerId = setInterval(clockTick, 1000);
@@ -38,20 +38,20 @@ function getQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
 
   // update title with current question
-  var titleEl = document.getElementById("question-title");
+  var titleEl = document.getElementById('question-title');
   titleEl.textContent = currentQuestion.title;
 
   // clear out any old question choices
-  choicesEl.innerHTML = "";
+  choicesEl.innerHTML = '';
 
   // loop over choices
   currentQuestion.choices.forEach(function(choice, i) {
     // create new button for each choice
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", choice);
+    var choiceNode = document.createElement('button');
+    choiceNode.setAttribute('class', 'choice');
+    choiceNode.setAttribute('value', choice);
 
-    choiceNode.textContent = i + 1 + ". " + choice;
+    choiceNode.textContent = i + 1 + '. ' + choice;
 
     // attach click event listener to each choice
     choiceNode.onclick = questionClick;
@@ -77,18 +77,18 @@ function questionClick() {
     // play "wrong" sound effect
     sfxWrong.play();
 
-    feedbackEl.textContent = "Wrong!";
+    feedbackEl.textContent = 'Wrong!';
   } else {
     // play "right" sound effect
     sfxRight.play();
 
-    feedbackEl.textContent = "Correct!";
+    feedbackEl.textContent = 'Correct!';
   }
 
   // flash right/wrong feedback on page for half a second
-  feedbackEl.setAttribute("class", "feedback");
+  feedbackEl.setAttribute('class', 'feedback');
   setTimeout(function() {
-    feedbackEl.setAttribute("class", "feedback hide");
+    feedbackEl.setAttribute('class', 'feedback hide');
   }, 1000);
 
   // move to next question
@@ -107,15 +107,15 @@ function quizEnd() {
   clearInterval(timerId);
 
   // show end screen
-  var endScreenEl = document.getElementById("end-screen");
-  endScreenEl.removeAttribute("class");
+  var endScreenEl = document.getElementById('end-screen');
+  endScreenEl.removeAttribute('class');
 
   // show final score
-  var finalScoreEl = document.getElementById("final-score");
+  var finalScoreEl = document.getElementById('final-score');
   finalScoreEl.textContent = time;
 
   // hide questions section
-  questionsEl.setAttribute("class", "hide");
+  questionsEl.setAttribute('class', 'hide');
 }
 
 function clockTick() {
@@ -134,10 +134,10 @@ function saveHighscore() {
   var initials = initialsEl.value.trim();
 
   // make sure value wasn't empty
-  if (initials !== "") {
+  if (initials !== '') {
     // get saved scores from localstorage, or if not any, set to empty array
     var highscores =
-      JSON.parse(window.localStorage.getItem("highscores")) || [];
+      JSON.parse(window.localStorage.getItem('highscores')) || [];
 
     // format new score object for current user
     var newScore = {
@@ -147,16 +147,16 @@ function saveHighscore() {
 
     // save to localstorage
     highscores.push(newScore);
-    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    window.localStorage.setItem('highscores', JSON.stringify(highscores));
 
     // redirect to next page
-    window.location.href = "highscores.html";
+    window.location.href = 'highscores.html';
   }
 }
 
 function checkForEnter(event) {
   // "13" represents the enter key
-  if (event.key === "Enter") {
+  if (event.key === 'Enter') {
     saveHighscore();
   }
 }
--- 01-Class-Content/04-Web-APIs/02-Homework/Master/assets/js/scores.js
+++ 01-Class-Content/04-Web-APIs/02-Homework/Master/assets/js/scores.js
@@ -1,6 +1,6 @@
 function printHighscores() {
   // either get scores from localstorage or set to empty array
-  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
 
   // sort highscores by score property in descending order
   highscores.sort(function(a, b) {
@@ -9,21 +9,21 @@ function printHighscores() {
 
   highscores.forEach(function(score) {
     // create li tag for each high score
-    var liTag = document.createElement("li");
-    liTag.textContent = score.initials + " - " + score.score;
+    var liTag = document.createElement('li');
+    liTag.textContent = score.initials + ' - ' + score.score;
 
     // display on page
-    var olEl = document.getElementById("highscores");
+    var olEl = document.getElementById('highscores');
     olEl.appendChild(liTag);
   });
 }
 
 function clearHighscores() {
-  window.localStorage.removeItem("highscores");
+  window.localStorage.removeItem('highscores');
   window.location.reload();
 }
 
-document.getElementById("clear").onclick = clearHighscores;
+document.getElementById('clear').onclick = clearHighscores;
 
 // run function when page loads
 printHighscores();
