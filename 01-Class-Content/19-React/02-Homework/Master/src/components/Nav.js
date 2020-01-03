8856c835be83292a9eea3c8e4b866b569232f501
John Desrosiers <john.desro@gmail.com>
2020-01-03 08:59:31 -0800

    Fixes #385 unit 19 homework ui controls
    This commit addresses some UI "bugs" in the solution for the unit 19
    homework assigment.
    
    Removes non-functioning Twitter Bootstrap collapse for the Nav
    component. Now the search input is always visible.
    
    Removes DOB input controls that did not do anything.
    
    Removes "Search" submit button that did not do anything.
    
    Refactor the `DateArea` component in the following ways to simplify
    code:
    
    - Extract the `handleSearchChange` method from `DateArea.state` to be a
      method of `DateArea`.
    - Extract the `handleSort` method from `DateArea.state` to be a method
      of `DateArea`.
    - `DateArea.headings` replaces `DateArea.state.headings`.
      (`state.headings` is never updated.)
    
    Closes issue #385

--- 01-Class-Content/19-React/02-Homework/Master/src/components/Nav.js
+++ 01-Class-Content/19-React/02-Homework/Master/src/components/Nav.js
@@ -4,31 +4,9 @@ import "../styles/Nav.css";
 
 function Nav({ handleSearchChange }) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse row" id="navbarNav">
-        <div className="input-group col-8">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="">
-              DOB
-            </span>
-          </div>
-          <input type="date" className="form-control" />
-          <input type="date" className="form-control" />
-        </div>
-        <div className="search-area col-4">
-          <SearchBox handleSearchChange={handleSearchChange} />
-        </div>
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="navbar-collapse row" id="navbarNav">
+        <SearchBox handleSearchChange={handleSearchChange} />
       </div>
     </nav>
   );
--- 01-Class-Content/19-React/02-Homework/Master/src/components/SearchBox.js
+++ 01-Class-Content/19-React/02-Homework/Master/src/components/SearchBox.js
@@ -6,15 +6,12 @@ function SearchBox({ handleSearchChange }) {
     <div className="searchbox">
       <form className="form-inline">
         <input
-          className="form-control mr-sm-2"
+          className="form-control"
           type="search"
           placeholder="Search"
           aria-label="Search"
           onChange={e => handleSearchChange(e)}
         />
-        <button className="btn my-2 my-sm-0" type="submit">
-          Search
-        </button>
       </form>
     </div>
   );
