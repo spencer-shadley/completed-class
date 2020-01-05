10455ec56bfbf3fba6bcf624b16ede0b7b1058a5
Elrey Belmonti <elbelmonti@gmail.com>
2020-01-05 08:37:21 -0800

    refactor loading gif

--- 06-server-side-apis/ajax-review/script.js
+++ 06-server-side-apis/ajax-review/script.js
@@ -1,53 +1,53 @@
 $(document).ready(function() {
-  // var ajaxCount = 0;
-  // $("main, #header, select, #myCarousel").hide();
+  var ajaxCount = 0;
 
-  // $(document).ajaxComplete(function() {
-  //   ++ajaxCount;
-  //   if (ajaxCount === 3) {
-  //     $("#loading").hide();
-  //     $("main, #header, select, #myCarousel").show();
-  //   }
-  // });
+  $(document).ajaxComplete(function() {
+    ++ajaxCount;
+    if (ajaxCount === 5) {
+      $('#loading').hide();
+      $('main').removeClass('d-none');
+      $('main').addClass('d-flex');
+    }
+  });
 
   var categories = [
-    "dev",
-    "movie",
-    "food",
-    "celebrity",
-    "science",
-    "political",
-    "sport",
-    "religion",
-    "animal",
-    "music",
-    "history",
-    "travel",
-    "career",
-    "money",
-    "fashion"
+    'dev',
+    'movie',
+    'food',
+    'celebrity',
+    'science',
+    'political',
+    'sport',
+    'religion',
+    'animal',
+    'music',
+    'history',
+    'travel',
+    'career',
+    'money',
+    'fashion'
   ];
 
-  var controls = $($("#controls").html());
+  var controls = $($('#controls').html());
 
-  var currentDate = moment().format("dddd MMM. Do, YYYY");
-  $("#current-date").text(currentDate);
+  var currentDate = moment().format('dddd MMM. Do, YYYY');
+  $('#current-date').text(currentDate);
 
   chuckGifs();
 
   function chuckGifs() {
     var queryURL =
-      "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=chuck%norris";
+      'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=chuck%norris';
     $.ajax({
       url: queryURL,
-      method: "GET"
+      method: 'GET'
     }).then(function(response) {
       var results = response.data;
-      var category = $("#current-category")
+      var category = $('#current-category')
         .text()
         .toLowerCase();
 
-      $(".carousel-inner").empty();
+      $('.carousel-inner').empty();
 
       for (var i = 0; i < results.length; i++) {
         var gifUrl = results[i].images.downsized.url;
@@ -55,24 +55,24 @@ $(document).ready(function() {
 
         var carouselItemEl = createCarouselItem(i);
 
-        var img = $("<img>");
+        var img = $('<img>');
         img.attr({
           src: gifUrl,
-          class: "d-block w-100",
-          alt: "gif",
-          "data-still": stillUrl,
-          "data-animate": gifUrl,
-          "data-state": "animate"
+          class: 'd-block w-100',
+          alt: 'gif',
+          'data-still': stillUrl,
+          'data-animate': gifUrl,
+          'data-state': 'animate'
         });
 
         carouselItemEl.append(img);
 
         chuckFact(carouselItemEl, category);
 
-        $(".carousel-inner").append(carouselItemEl);
+        $('.carousel-inner').append(carouselItemEl);
       }
 
-      $(".carousel-inner").append(controls);
+      $('.carousel-inner').append(controls);
     });
   }
 
@@ -80,37 +80,37 @@ $(document).ready(function() {
     var carouselItemEl =
       index === 0
         ? $("<div class='carousel-item active' >")
-        : $("<div class=carousel-item >");
+        : $('<div class=carousel-item >');
     return carouselItemEl;
   }
 
   function chuckFact(element, category) {
     var queryURL =
-      "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/categories";
+      'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/categories';
     var settings = {
       async: true,
       crossDomain: true,
       url:
-        "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random?category=" +
+        'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random?category=' +
         category,
-      method: "GET",
+      method: 'GET',
       headers: {
-        "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
-        "x-rapidapi-key": "26fcba3080msh3a2259b034b4c3dp1f9e01jsn7b1d1f231b17",
-        accept: "application/json"
+        'x-rapidapi-host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
+        'x-rapidapi-key': '26fcba3080msh3a2259b034b4c3dp1f9e01jsn7b1d1f231b17',
+        accept: 'application/json'
       }
     };
     $.ajax(settings).then(function(response) {
-      var captionEl = $("<div></div>");
-      var buttonEl = $("<button></button>");
+      var captionEl = $('<div></div>');
+      var buttonEl = $('<button></button>');
       buttonEl
-        .addClass("btn")
-        .attr("type", "button")
-        .text("Still");
+        .addClass('btn')
+        .attr('type', 'button')
+        .text('Still');
 
       appendFists(buttonEl);
       buttonEl.click(gifAnimateControl);
-      captionEl.addClass("carousel-caption d-none d-md-block");
+      captionEl.addClass('carousel-caption d-none d-md-block');
       var text = response.value;
       var textEl = $(`<h2>${text}</h2>`);
       captionEl.append([buttonEl, textEl]);
@@ -122,30 +122,30 @@ $(document).ready(function() {
     var imgEl = $(this)
       .parent()
       .parent()
-      .children("img");
-    var state = imgEl.attr("data-state");
+      .children('img');
+    var state = imgEl.attr('data-state');
 
-    if (state === "still") {
+    if (state === 'still') {
       imgEl.attr({
-        src: imgEl.attr("data-animate"),
-        "data-state": "animate"
+        src: imgEl.attr('data-animate'),
+        'data-state': 'animate'
       });
 
-      appendFists($(this).text("Still"));
-    } else if (state === "animate") {
+      appendFists($(this).text('Still'));
+    } else if (state === 'animate') {
       imgEl.attr({
-        src: imgEl.attr("data-still"),
-        "data-state": "still"
+        src: imgEl.attr('data-still'),
+        'data-state': 'still'
       });
-      appendFists($(this).text("Animate"));
+      appendFists($(this).text('Animate'));
     }
   }
 
   function appendFists(element) {
-    element.append("<p>👊🏻👊🏼👊🏽👊🏾👊🏿</p>");
+    element.append('<p>👊🏻👊🏼👊🏽👊🏾👊🏿</p>');
   }
 
-  var selectEl = $("select#categories");
+  var selectEl = $('select#categories');
   $.each(categories, function(i, category) {
     var optionEl = $(`<option value=${category}>${category}</option>`);
     selectEl.append(optionEl);
@@ -153,10 +153,10 @@ $(document).ready(function() {
 
   selectEl.change(function() {
     var selectedCategory = $(this)
-      .children("option:selected")
+      .children('option:selected')
       .val();
 
-    $("#current-category").text(selectedCategory);
+    $('#current-category').text(selectedCategory);
     chuckGifs();
   });
 });
