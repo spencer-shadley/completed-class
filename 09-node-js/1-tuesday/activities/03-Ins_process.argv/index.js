55852236e44a60bf4cf043be7d010b4840a93134
Spencer Shadley <spencershadley@utexas.edu>
2020-01-13 23:09:53 -0800

    updated wording of comment

--- 09-node-js/1-tuesday/activities/03-Ins_process.argv/index.js
+++ 09-node-js/1-tuesday/activities/03-Ins_process.argv/index.js
@@ -1,5 +1,4 @@
 // more info - https://nodejs.org/api/process.html#process_process_argv
 
-// returns an array of command line arguments
 console.log(process.argv);
 console.log(JSON.parse(process.argv[2]).hello);
