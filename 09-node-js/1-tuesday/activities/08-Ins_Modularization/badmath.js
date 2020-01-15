155bbf63851b8601359c3691cc4a6e283f5e7756
Spencer Shadley <spencershadley@utexas.edu>
2020-01-14 22:28:55 -0800

    added documentation

--- 09-node-js/1-tuesday/activities/08-Ins_Modularization/badmath.js
+++ 09-node-js/1-tuesday/activities/08-Ins_Modularization/badmath.js
@@ -5,6 +5,7 @@ var predictable = function() {
 };
 
 // module.exports is an object we use to store variables or methods
+// documentation: https://nodejs.org/api/modules.html#modules_module_exports
 module.exports = {
   pie: pie,
   predictable: predictable
