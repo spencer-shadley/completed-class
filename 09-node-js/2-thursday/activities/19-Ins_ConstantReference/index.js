87d76becb1fb0bd1acdc43d7eff0ce53e8700e89
Spencer Shadley <spencershadley@utexas.edu>
2020-01-15 23:09:55 -0800

    uncommented const code blocks

--- 09-node-js/2-thursday/activities/19-Ins_ConstantReference/index.js
+++ 09-node-js/2-thursday/activities/19-Ins_ConstantReference/index.js
@@ -6,57 +6,52 @@ age++; // TypeError: Assignment to constant variable.
 
 // ==========================================================================
 
-// const name = "";
+const name = '';
 
-// name = "Cherie"; // TypeError: Assignment to constant variable.
+name = 'Cherie'; // TypeError: Assignment to constant variable.
 
 // ==========================================================================
 
 // 2. const doesn't mean `constant value`, instead means `constant reference`
 
 // Unlike primitive data types, objects and arrays are passed by reference, rather than passed by value
-// const beatles = ["John", "Paul", "Ringo"];
-// beatles.push("George");
+const beatles = ['John', 'Paul', 'Ringo'];
+beatles.push('George');
 
 // This works because by updating an array's contents, we aren't changing the reference to the underlying array
-// console.log(beatles); // Prints `["John", "Paul", "Ringo", "George"]`
+console.log(beatles); // Prints `["John", "Paul", "Ringo", "George"]`
 
-// console.log(beatles);
+console.log(beatles);
 
-// const person = { name: "Brianna", age: 11 };
-// person.age++;
-// person.favoriteMovie = "Spider-Man";
-// person.name = "Carla";
+const person = { name: 'Brianna', age: 11 };
+person.age++;
+person.favoriteMovie = 'Spider-Man';
+person.name = 'Carla';
 
-// console.log(person); // Prints `{ name: 'Carla', age: 12, favoriteMovie: 'Spider-Man' }`
+console.log(person); // Prints `{ name: 'Carla', age: 12, favoriteMovie: 'Spider-Man' }`
 
 // ==========================================================================
 
 // 3. While we can MODIFY arrays and objects that are using `const`, we can't reassign them
 
-// const item = {
-//   id: 23,
-//   title: "Underwater Basket-Weaving DVD",
-//   price: "$17.99"
-// };
+const item = {
+  id: 23,
+  title: 'Underwater Basket-Weaving DVD',
+  price: '$17.99'
+};
 
-// item.price = "$1.99";
+item.price = '$1.99';
 
-// console.log(item);
+console.log(item);
 
-// const item = {
-//   id: 11,
-//   title: "Underwater Basket-Weaving Shoes",
-//   price: "$101.43"
-// }; // TypeError: Assignment to constant variable.
+const item = {
+  id: 11,
+  title: 'Underwater Basket-Weaving Shoes',
+  price: '$101.43'
+}; // TypeError: Assignment to constant variable.
 
-// const ninjaTurtles = [];
+const ninjaTurtles = [];
 
 // The same rules apply to arrays, we can MODIFY them, but not completely reassign them
 
-// ninjaTurtles = [
-//   "Michaelangelo",
-//   "Leonardo",
-//   "Raphael",
-//   "Donatello"
-// ]; // TypeError: Assignment to constant variable.
+ninjaTurtles = ['Michaelangelo', 'Leonardo', 'Raphael', 'Donatello']; // TypeError: Assignment to constant variable.
