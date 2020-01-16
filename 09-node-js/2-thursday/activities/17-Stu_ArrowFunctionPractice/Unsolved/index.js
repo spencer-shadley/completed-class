7e013597374f470b8173040ab843b59344739d31
Spencer Shadley <spencershadley@utexas.edu>
2020-01-15 22:54:18 -0800

    updated function syntax

--- 09-node-js/2-thursday/activities/17-Stu_ArrowFunctionPractice/Unsolved/index.js
+++ 09-node-js/2-thursday/activities/17-Stu_ArrowFunctionPractice/Unsolved/index.js
@@ -1,5 +1,5 @@
 // 1. funnyCase makes each letter in a string the opposite case as the one before
-var funnyCase = function(string) {
+var funnyCase = function funnyCase(string) {
   var newString = '';
   for (var i = 0; i < string.length; ++i) {
     if (i % 2 === 0) newString += string[i].toLowerCase();
@@ -14,14 +14,14 @@ console.log(funnyCase("You can't just do whatever you want all the time!"));
 // --------------------------------------------------------------------------
 
 // 2. Map lets you loop over an array and modify the elements inside
-var map = function(arr, cb) {
+function map(arr, cb) {
   var result = [];
   for (var index = 0; index < arr.length; index++) {
     var currentElement = arr[index];
     result.push(cb(currentElement, index));
   }
   return result;
-};
+}
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
