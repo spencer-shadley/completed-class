b1cec323d91716c9747964d8d81901af8e2bcc51
Spencer Shadley <spencershadley@utexas.edu>
2020-01-15 23:07:10 -0800

    uncommented out blocks of code

--- 09-node-js/2-thursday/activities/18-Ins_LetAndBlockScope/index.js
+++ 09-node-js/2-thursday/activities/18-Ins_LetAndBlockScope/index.js
@@ -24,46 +24,45 @@ console.log(j); // prints 42
 
 // 2. When using while loops, any values we create inside exist outside of the while-loop block
 
-// var count = 0;
+var count = 0;
 
-// while (count < 5) {
-//   var tripled = count * 3;
-//   count++;
-// }
+while (count < 5) {
+  var tripled = count * 3;
+  count++;
+}
 
-// console.log(tripled); // Prints 12
+console.log(tripled); // Prints 12
 
 // ==========================================================================
 
 // 3. When using let, values defined inside of the while-loop block don't exist outside of it
 
-// let c = 0;
+let c = 0;
 
-// while (c < 5) {
-//   let quadrupled = c * 3;
-//   c++;
-// }
+while (c < 5) {
+  let quadrupled = c * 3;
+  c++;
+}
 
-// console.log(quadrupled); // ReferenceError: quadrupled is not defined
+console.log(quadrupled); // ReferenceError: quadrupled is not defined
 
 // ==========================================================================
 
 // 4. When writing conditionals, values defined inside the conditional block exist outside of it
 
-// if (true) {
-//   var favoriteColor = "red";
-// }
+if (true) {
+  var favoriteColor = 'red';
+}
 
-// console.log(favoriteColor); // Prints `red`
+console.log(favoriteColor); // Prints `red`
 
 // When using let, values defined inside of a conditional block don't exist outside
 
-// let favoriteFood;
+let favoriteFood;
 
-// if (true) {
-//   favoriteFood = "pizza";
-// }
+if (true) {
+  favoriteFood = 'pizza';
+}
 
 // This works since favoriteColor is not defined inside of a block
-// console.log(favoriteFood);
-// Prints `pizza`
+console.log(favoriteFood); // Prints `pizza`
