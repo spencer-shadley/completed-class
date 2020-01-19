8147928a801252d76370212877e945be1383343b
Spencer Shadley <spencershadley@utexas.edu>
2020-01-19 14:24:30 -0800

    added strict; switched to modern ES6

--- 09-node-js/2-thursday/activities/27-Ins_Rest_and_Spread/index.js
+++ 09-node-js/2-thursday/activities/27-Ins_Rest_and_Spread/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // without rest
 function add(x, y) {
   return x + y;
@@ -7,7 +9,7 @@ console.log(add(1, 2, 3, 4, 5)); // => 3
 // 1. rest
 function add(...nums) {
   let sum = 0;
-  for (let num of nums) sum += num;
+  for (const num of nums) sum += num;
   return sum;
 }
 
