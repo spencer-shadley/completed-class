32e0a96eae7fa79ef040f08d14c44f9ab5054364
Spencer Shadley <spencershadley@utexas.edu>
2020-01-18 23:48:07 -0800

    updated comment

--- 09-node-js/2-thursday/activities/23-Ins_Template_Literals/index.js
+++ 09-node-js/2-thursday/activities/23-Ins_Template_Literals/index.js
@@ -8,6 +8,7 @@ const arya = {
 const greeting = `My name is ${arya.first}!
 I am loyal to ${arya.allegiance}.`;
 
-console.log(greeting); // prints
+console.log(greeting);
+// prints:
 // My name is Arya!
 // I am loyal to House Stark.
