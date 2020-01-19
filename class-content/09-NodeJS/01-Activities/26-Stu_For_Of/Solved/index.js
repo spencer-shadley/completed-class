76afcf8211af9470446f81595b5cc90ea5b9b24d
Spencer Shadley <spencershadley@utexas.edu>
2020-01-19 14:18:48 -0800

    rewrote solution

--- class-content/09-NodeJS/01-Activities/26-Stu_For_Of/Solved/index.js
+++ class-content/09-NodeJS/01-Activities/26-Stu_For_Of/Solved/index.js
@@ -1,5 +1,27 @@
-const songs = document.querySelectorAll('ul > li');
+const songs = document.querySelectorAll('ol > li');
+const forType = 'for...of'; // update me to whichever for loop you want to try!
 
-for (const song of songs) {
+// more info on loop types - https://stackoverflow.com/questions/9329446/for-each-over-an-array-in-javascript
+switch (forType) {
+  case 'for...of':
+    for (const song of songs) {
+      addGreenClass(song);
+    }
+    break;
+  case 'for':
+    for (var i = 0; i < songs.length; ++i) {
+      addGreenClass(songs[i]);
+    }
+    break;
+  case 'forEach':
+    songs.forEach(addGreenClass);
+    break;
+  case 'for-in':
+    console.error("for-in is a dangerous loop type, don't use it!");
+  default:
+    console.error('unknown for type', forType);
+}
+
+function addGreenClass(song) {
   song.classList.add('green');
 }
