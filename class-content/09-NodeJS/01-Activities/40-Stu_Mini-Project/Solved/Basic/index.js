e6adbfc1f283a3d896c5d8be8615fbbec3836387
Spencer Shadley <spencershadley@utexas.edu>
2020-01-19 16:24:51 -0800

    rewrote solution

--- class-content/09-NodeJS/01-Activities/40-Stu_Mini-Project/Solved/Basic/index.js
+++ class-content/09-NodeJS/01-Activities/40-Stu_Mini-Project/Solved/Basic/index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const inquirer = require('inquirer');
 const fs = require('fs');
--- class-content/09-NodeJS/01-Activities/40-Stu_Mini-Project/Solved/Bonus/index.js
+++ class-content/09-NodeJS/01-Activities/40-Stu_Mini-Project/Solved/Bonus/index.js
@@ -1,44 +1,14 @@
-'use strict'
+'use strict';
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const prompts = require('./prompts');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?'
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?'
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?'
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username'
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.'
-    }
-  ]);
+  return inquirer.prompt(prompts);
 }
 
 function generateHTML(answers) {
@@ -68,7 +38,7 @@ function generateHTML(answers) {
 }
 
 async function init() {
-  console.log('hi');
+  console.log('initializing...');
   try {
     const answers = await promptUser();
 
