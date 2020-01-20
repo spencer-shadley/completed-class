2af731672c2e77e7646dd74b62785f5e448dd1ea
Spencer Shadley <spencershadley@utexas.edu>
2020-01-19 22:34:58 -0800

    added unit 9 algorithms

---
+++ 09-node-js/algorithms/03-camel-case/Unsolved/test/camel-case.test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var expect = chai.expect;
+
+describe('camelCase', function() {
+  it("should return 'helloWorld' when given 'Hello World'", function() {
+    var str = 'Hello World';
+
+    var result = camelCase(str);
+
+    expect(result).to.eql('helloWorld');
+  });
+
+  it("should return 'theRainInSpainFallsMainlyOnThePlain' when given 'The rain in spain falls mainly on the plain'", function() {
+    var str = 'The rain in spain falls mainly on the plain';
+
+    var result = camelCase(str);
+
+    expect(result).to.eql('theRainInSpainFallsMainlyOnThePlain');
+  });
+});
