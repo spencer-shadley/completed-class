04e4cbc79db68ba1b0cbeb3246e9ff2ab244585d
Spencer Shadley <spencershadley@utexas.edu>
2020-01-23 18:12:27 -0800

    updated solution

--- lesson-plans/09-Week_NodeJS/02-Day_ES6/Solved/test/reverse-no-built-in.test.js
+++ lesson-plans/09-Week_NodeJS/02-Day_ES6/Solved/test/reverse-no-built-in.test.js
@@ -2,36 +2,33 @@
 
 var expect = chai.expect;
 
-describe('reverse'),
-  function() {
-    describe('reverse characters', function() {
-      it("should return '!dlroW olleH' when given 'Hello World!'", function() {
-        var result = reverse('Hello World!');
-        expect(result).to.eql('!dlroW olleH');
-      });
+describe('reverse', function() {
+  it("should return '!dlroW olleH' when given 'Hello World!'", function() {
+    var result = reverse('Hello World!');
+    expect(result).to.eql('!dlroW olleH');
+  });
 
-      it("should return '!dlroW eybooG' when given 'Goodbye World!'", function() {
-        var result = reverse('Goodbye World!');
-        expect(result).to.eql('!dlroW eybdooG');
-      });
-    });
+  it("should return '!dlroW eybooG' when given 'Goodbye World!'", function() {
+    var result = reverse('Goodbye World!');
+    expect(result).to.eql('!dlroW eybdooG');
+  });
+});
 
-    // bonus
-    describe('reverse words', function() {
-      it('should reverse a sentence', function() {
-        var actual = reverseWords('Hello world!');
-        expect(actual).to.eql('world! Hello');
-      });
+// bonus
+describe('reverse words', function() {
+  it('should reverse a sentence', function() {
+    var actual = reverseWords('Hello world!');
+    expect(actual).to.eql('world! Hello');
+  });
 
-      it('should return the same word when given a single word', function() {
-        var input = 'Hello';
-        var actual = reverseWords(input);
-        expect(actual).to.eql(input);
-      });
+  it('should return the same word when given a single word', function() {
+    var input = 'Hello';
+    var actual = reverseWords(input);
+    expect(actual).to.eql(input);
+  });
 
-      it('should gracefully handle null', function() {
-        var actual = reverseWords(null);
-        expect(actual).to.eql(undefined);
-      });
-    });
-  };
+  it('should gracefully handle null', function() {
+    var actual = reverseWords(null);
+    expect(actual).to.eql(undefined);
+  });
+});
