492a9451bb6da8ab7c9df9b0a73aee2fb1a31f8f
Spencer Shadley <spencershadley@utexas.edu>
2020-01-23 21:04:18 -0800

    switched to arrows

--- class-content/09-NodeJS/01-Activities/28-Stu_Rest_and_Spread/Solved/index.js
+++ class-content/09-NodeJS/01-Activities/28-Stu_Rest_and_Spread/Solved/index.js
@@ -8,14 +8,14 @@ console.log(newSongs); // => ["Creep", "Everlong", "Bulls On Parade", "Song 2", 
 
 // 2.  Modify the add function so that is uses the rest operator and maintains the same functionality. You should still be able to pass in any number of arguments.
 
-function add(x, y, z) {
+const add = (x, y, z) => {
   const array = [x, y, z];
   return array.reduce((a, b) => a + b, 0);
-}
+};
 console.log(add(1, 2, 3)); // 6
 
-function addSpread(...nums) {
+const addSpread = (...nums) => {
   return nums.reduce((a, b) => a + b); // you don't have to change this line of code but look up reduce if you're not sure what it does here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-}
+};
 console.log(addSpread(1, 2, 3)); // 6
 console.log(addSpread(1, 2, 3, 4, 100)); // 110
