950795b6834e05a840a4af383696d86404b1257c
Spencer Shadley <spencershadley@utexas.edu>
2020-01-24 23:18:34 -0800

    ES6 ified

--- 09-node-js/3-saturday-async-js/activities/32-Ins_Axios-Demo/solved/index.js
+++ 09-node-js/3-saturday-async-js/activities/32-Ins_Axios-Demo/solved/index.js
@@ -1,9 +1,7 @@
-'use strict'
+'use strict';
 
 const axios = require('axios');
 
 axios
   .get('https://www.omdbapi.com/?t=The%20Matrix&apikey=trilogy')
-  .then(function(res) {
-    console.log(res.data);
-  });
+  .then(res => console.log(res.data));
