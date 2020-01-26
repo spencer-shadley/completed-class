97500ddf9143b9e31e75776c65b6845fb049332e
Spencer Shadley <spencershadley@utexas.edu>
2020-01-26 11:44:14 -0800

    added unit 10 activity changes from fork

--- 01-Class-Content/10-OOP/01-Activities/03-Stu_Mini-Bank/Unsolved/minibank.js
+++
@@ -1,9 +0,0 @@
-function MiniBank(balance) {
-  this.balance = balance;
-  this.getBalance = function() {
-    return this.balance;
-  },
-  this.printBalance = function() {
-    console.log(`Balance: ${this.getBalance()}`);
-  };
-}
