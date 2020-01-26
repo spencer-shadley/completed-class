efec51dea556cd32482e2182ed96a686c9829761
Spencer Shadley <spencershadley@utexas.edu>
2020-01-26 11:31:06 -0800

    deleted already covered material from merge

--- 01-Class-Content/20-State/01-Activities/14-Stu_MultipleContexts/Unsolved/src/components/CardTitleText/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import UserContext from "../../utils/userContext";
-
-function CardTitleText() {
-  const { user } = useContext(UserContext);
-  return <h2>{user.title}</h2>;
-}
-
-export default CardTitleText;
