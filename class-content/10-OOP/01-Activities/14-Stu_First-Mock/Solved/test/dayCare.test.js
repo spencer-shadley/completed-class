190959e18b1cefe1d707e56abd8c8493d6fff6e7
Spencer Shadley <spencershadley@utexas.edu>
2020-01-27 15:32:22 -0800

    renamed to mockConsoleLog

--- class-content/10-OOP/01-Activities/14-Stu_First-Mock/Solved/test/dayCare.test.js
+++ class-content/10-OOP/01-Activities/14-Stu_First-Mock/Solved/test/dayCare.test.js
@@ -26,24 +26,24 @@ describe('DayCare', () => {
     it("should not add a child over the 'ageLimit'", () => {
       const child = new Child('Tammy', 8);
       const dayCare = new DayCare();
-      const mock = jest.spyOn(console, 'log');
-      mock.mockImplementation(() => {});
+      const mockConsoleLog = jest.spyOn(console, 'log');
+      mockConsoleLog.mockImplementation(() => {});
 
       dayCare.addChild(child);
 
       expect(dayCare.children.length).toEqual(0);
-      expect(mock).toBeCalledWith(
+      expect(mockConsoleLog).toBeCalledWith(
         'Unable to add child, they are over the age limit'
       );
 
-      mock.mockRestore();
+      mockConsoleLog.mockRestore();
     });
 
     it('should not add a child if already at capacity', () => {
       const dayCare = new DayCare();
       const child = new Child('Alice', 4);
-      const mock = jest.spyOn(console, 'log');
-      mock.mockImplementation(() => {});
+      const mockConsoleLog = jest.spyOn(console, 'log');
+      mockConsoleLog.mockImplementation(() => {});
       dayCare.children = [
         new Child('Tammy', 1),
         new Child('Mark', 2),
@@ -53,21 +53,23 @@ describe('DayCare', () => {
       dayCare.addChild(child);
 
       expect(dayCare.children.length).toEqual(3);
-      expect(mock).toBeCalledWith('At capacity, unable to add more children');
+      expect(mockConsoleLog).toBeCalledWith(
+        'At capacity, unable to add more children'
+      );
 
-      mock.mockRestore();
+      mockConsoleLog.mockRestore();
     });
 
     it('should throw an error if not provided a Child object as an argument', () => {
       const err = new Error(
         "Expected parameter 'child' to be an instance of Child"
       );
-      const cb = () => {
+      const callback = () => {
         const dayCare = new DayCare();
         dayCare.addChild();
       };
 
-      expect(cb).toThrowError(err);
+      expect(callback).toThrowError(err);
     });
   });
 
@@ -77,10 +79,10 @@ describe('DayCare', () => {
       const child1 = new Child('Tammy', 1);
       const child2 = new Child('Mark', 2);
       const child3 = new Child('Alvin', 1);
-      const mock = jest.spyOn(console, 'log');
+      const mockConsoleLog = jest.spyOn(console, 'log');
       dayCare.children = [child1, child2, child3];
 
-      mock.mockImplementation(() => {});
+      mockConsoleLog.mockImplementation(() => {});
       const removed = dayCare.pickupChild(child2.name);
 
       expect(removed).toBe(child2);
@@ -88,9 +90,11 @@ describe('DayCare', () => {
       expect(
         dayCare.children.some(child => child.name === child2.name)
       ).toEqual(false);
-      expect(mock).toBeCalledWith(`Picked up ${child2.name} from day care`);
+      expect(mockConsoleLog).toBeCalledWith(
+        `Picked up ${child2.name} from day care`
+      );
 
-      mock.mockRestore();
+      mockConsoleLog.mockRestore();
     });
 
     it("should return undefined and remove no children if child is not in 'children'", () => {
@@ -98,17 +102,17 @@ describe('DayCare', () => {
       const child1 = new Child('Tammy', 1);
       const child2 = new Child('Mark', 2);
       const child3 = new Child('Alvin', 1);
-      const mock = jest.spyOn(console, 'log');
+      const mockConsoleLog = jest.spyOn(console, 'log');
       dayCare.children = [child1, child2, child3];
 
-      mock.mockImplementation(() => {});
+      mockConsoleLog.mockImplementation(() => {});
       const removed = dayCare.pickupChild('Fred');
 
       expect(typeof removed).toEqual('undefined');
       expect(dayCare.children).toEqual([child1, child2, child3]);
-      expect(mock).toBeCalledWith('Child not found');
+      expect(mockConsoleLog).toBeCalledWith('Child not found');
 
-      mock.mockRestore();
+      mockConsoleLog.mockRestore();
     });
   });
 });
