263baaf2db37b3485559b53fb181b1ead1c34881
Spencer Shadley <spencershadley@utexas.edu>
2020-01-27 14:31:39 -0800

    switched to ES6

--- class-content/10-OOP/01-Activities/10-Stu_Pass-Tests/Solved/algo.js
+++ class-content/10-OOP/01-Activities/10-Stu_Pass-Tests/Solved/algo.js
@@ -12,13 +12,10 @@ Algo.prototype.isPalindrome = function(str) {
   return this.reverse(str) === str;
 };
 
-Algo.prototype.capitalize = function(str) {
-  return str
+Algo.prototype.capitalize = str =>
+  str
     .split(' ')
-    .map(word => {
-      return word.substring(0, 1).toUpperCase() + word.substring(1);
-    })
+    .map(word => word.substring(0, 1).toUpperCase() + word.substring(1))
     .join(' ');
-};
 
 module.exports = Algo;
