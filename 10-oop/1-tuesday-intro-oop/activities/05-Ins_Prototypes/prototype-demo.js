9fdb676c18321995fc9b60d653831834f69933b4
Spencer Shadley <spencershadley@utexas.edu>
2020-01-26 17:08:40 -0800

    added more examples; fixed declarations

--- 10-oop/1-tuesday-intro-oop/activities/05-Ins_Prototypes/prototype-demo.js
+++ 10-oop/1-tuesday-intro-oop/activities/05-Ins_Prototypes/prototype-demo.js
@@ -1,29 +1,31 @@
 'use strict';
 
+// 0.
+
+const hello = 'Hello';
+console.log(hello);
+console.log(hello.toLowerCase()); // where is toLowerCase()?
+
+const num = 1337;
+console.log(num);
+console.log(num.toString()); // where is toString()?
+
 // 1.
-myArray = [2, 4, 6, 8];
+const myArray = [2, 4, 6, 8];
 console.log(myArray);
 
 myArray.forEach(num => console.log(num));
 
-myArray.map(x => console.log(x * 2));
-//
+myArray.map(num => console.log(num * 2));
 
 // 2.
-myObject = {
+const myObject = {
   name: 'Eric',
   age: 28,
   occupation: 'Curriculum Dev'
 };
 
 console.log(myObject);
-
-console.log('Hello');
-console.log('Hello'.toLowerCase());
-
-console.log(1337);
-console.log((1337).toString());
-//
 
 // 3.
 function Movie(title, releaseYear) {
@@ -41,4 +43,3 @@ theShining.logInfo();
 console.log(theShining.hasOwnProperty('title'));
 console.log(theShining.hasOwnProperty('logInfo'));
 console.log(Movie.prototype.hasOwnProperty('logInfo'));
-//
