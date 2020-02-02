a149579ea1aa0e7c59927d7231948ef7a0f61eb8
Spencer Shadley <spencershadley@utexas.edu>
2020-02-02 12:43:12 -0800

    moved unit 11 stuff to gitlab

--- class-content/11-express/03-Algorithms/02-is-perfect-square/Unsolved/is-perfect-square.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
-// You may not use the built-in Math.sqrt method
-
-const isPerfectSquare = function(num) {};
