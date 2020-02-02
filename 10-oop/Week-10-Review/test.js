343d20d98e1126259dea85d45e16c6fc529ad0f4
Spencer Shadley <spencershadley@utexas.edu>
2020-02-01 22:11:07 -0800

    in class changes

--- 10-oop/Week-10-Review/test.js
+++ 10-oop/Week-10-Review/test.js
@@ -1,38 +1,43 @@
 // Encapsulation
 
 class Hedgehog {
-    constructor() {
-        this.speed = 10000; 
-        this.name = 'Sonic';
-        this.zoom = function () {
-            // both name and speed are accessible from here
-            console.log(`${this.name} zooms with the speed of ${this.speed} km per hour!`);
-        };
-
-        
-    }
+  constructor(name) {
+    this.speed = 10000;
+    this.name = name;
+    this.zoom = function() {
+      // both name and speed are accessible from here
+      console.log(
+        `${this.name} zooms with the speed of ${this.speed} km per hour!`
+      );
+    };
+    return Object.freeze({
+      speed: this.speed,
+      name: this.name,
+      zoom: this.zoom
+    });
+  }
 }
- 
-const sonic = new Hedgehog();
 
-// console.log(`Sonics name is ${sonic.name}`)
+const sonic = new Hedgehog('Sonic');
+
+// console.log(`Sonics name is ${sonic.name}`);
 // console.log(`Sonics speed is ${sonic.speed} km/hr`);
 // sonic.zoom();
 
-freezeRay = (target) => {
-    target.speed = 0;
-    console.log(`I have frozen ${target.name}!`)
-}
-// freezeRay(sonic);
-// console.log(`Sonics speed is ${sonic.speed} km/hr`);
+freezeRay = target => {
+  target.speed = 0;
+  console.log(`I have frozen ${target.name}!`);
+};
+freezeRay(sonic);
+console.log(`Sonics speed is ${sonic.speed} km/hr`);
 
 // INHERITANCE
 
 class Tree {
-    constructor(name) {
-        this.name = name;
-        this.trunk = true;
-    }
+  constructor(name) {
+    this.name = name;
+    this.trunk = true;
+  }
 }
 
 const bananaTree = new Tree('banana');
@@ -42,32 +47,38 @@ const bananaTree = new Tree('banana');
 // ABSTRACTION
 
 class Person {
-    constructor({firstName, lastName, job}) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.job = job;
-    }
+  constructor({ firstName, lastName, job }) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.job = job;
+  }
 }
 
 class Job {
-    constructor({company, position, salary}) {
-        this.company = company;
-        this.position = position;
-        this.salary = salary;
-    }
+  constructor({ company, position, salary }) {
+    this.company = company;
+    this.position = position;
+    this.salary = salary;
+  }
 }
 
 const Kawhi = new Person({
-    firstName: 'Kawhi', 
-    lastName: 'Leonard', 
-    job: new Job({company: 'NBA', position: 'AllStar', salary: '$$'})
+  firstName: 'Kawhi',
+  lastName: 'Leonard',
+  job: new Job({ company: 'NBA', position: 'AllStar', salary: '$$' })
 });
 
-// console.log(Kawhi);
+console.log(Kawhi);
 
-// Person.prototype.defense = function () {
-//     console.log(`${this.firstName} ${this.lastName} can play some great defense`)
-// }
+Person.prototype.defense = function() {
+  if (this.firstName === 'Kawhi') {
+    console.log(
+      `${this.firstName} ${this.lastName} can play some great defense`
+    );
+  } else {
+    console.log(`Try again next year`);
+  }
+};
 
-// Kawhi.defense();
-// console.log(Kawhi.job)
+Kawhi.defense();
+console.log(Kawhi.job)
