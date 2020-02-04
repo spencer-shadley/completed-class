1288cafbc43a404f3013cf9b07b2f5f71dfb499a
Spencer Shadley <spencershadley@utexas.edu>
2020-02-03 21:56:31 -0800

    added 11-3 drill and tests

--- 11-express/3-saturday-express-mini-project/preclass-drill/drill.md
+++ 11-express/3-saturday-express-mini-project/preclass-drill/drill.md
@@ -1,1 +1,33 @@
 # Pre-class Drill
+
+Write a function that takes an array of numbers and a callbackfunction as parameters. The function parameter should return true if the input meets a certain condition or false otherwise. Your function should run the function parameter on every element in the array parameter and, if it returns true, add the element to a new array. Return the new array.
+
+---
+
+Input
+
+[1,2,3] function(num){return num === 2}
+
+Output
+
+[2]
+
+---
+
+Input
+
+[1,2,3,4,5,6] function(num){return num % 2 === 0}
+
+Output
+
+[2,4,6]
+
+---
+
+Input
+
+[1,2,3,4,5,6] function(num){return num > 3}
+
+Output
+
+[4,5,6]
--- 11-express/3-saturday-express-mini-project/preclass-drill/test/drill.test.js
+++ 11-express/3-saturday-express-mini-project/preclass-drill/test/drill.test.js
@@ -1,7 +1,32 @@
 'use strict';
 
-var expect = chai.expect;
+const drill = require('../drill');
 
-describe('', function() {
-  it('should ', function() {});
+describe('filter', () => {
+  it('should filter a simple function', () => {
+    const inputArr = [1, 2, 3, 4];
+    const isEven = num => num % 2 === 0;
+
+    const actual = drill.filter(inputArr, isEven);
+
+    expect(actual).toEqual([2, 4]);
+  });
+
+  it('should filter a when all are true', () => {
+    const inputArr = [1, 2, 3, 4];
+    const alwaysTrue = _ => true;
+
+    const actual = drill.filter(inputArr, alwaysTrue);
+
+    expect(actual).toEqual([1, 2, 3, 4]);
+  });
+
+  it('should filter when none are true', () => {
+    const inputArr = [1, 2, 3, 4];
+    const alwaysFalse = _ => false;
+
+    const actual = drill.filter(inputArr, alwaysFalse);
+
+    expect(actual).toEqual([]);
+  });
 });
