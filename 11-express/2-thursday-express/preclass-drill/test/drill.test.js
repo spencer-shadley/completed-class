b6db9682ba906663090f048740a8685701b73920
Spencer Shadley <spencershadley@utexas.edu>
2020-02-03 21:45:32 -0800

    added tests

--- 11-express/2-thursday-express/preclass-drill/test/drill.test.js
+++ 11-express/2-thursday-express/preclass-drill/test/drill.test.js
@@ -1,7 +1,70 @@
 'use strict';
 
-var expect = chai.expect;
+const drill = require('../drill');
 
-describe('', function() {
-  it('should ', function() {});
+describe('printPyramid', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = jest.spyOn(console, 'log');
+    consoleLogSpy.mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleLogSpy.mockRestore();
+  });
+
+  it('should print a small odd sized pyramid', () => {
+    const expected = ` X
+XXX`;
+
+    drill.printByArray(3);
+
+    expect(consoleLogSpy).toBeCalledWith(expected);
+  });
+
+  it('should print a small even sized pyramid', () => {
+    const expected = ` XX
+XXXX`;
+
+    drill.printByArray(4);
+
+    expect(consoleLogSpy).toBeCalledWith(expected);
+  });
+
+  it('should print a large odd sized pyramid', () => {
+    const expected = `          X
+         XXX
+        XXXXX
+       XXXXXXX
+      XXXXXXXXX
+     XXXXXXXXXXX
+    XXXXXXXXXXXXX
+   XXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXX
+ XXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXX`;
+
+    drill.printByArray(21);
+
+    expect(consoleLogSpy).toBeCalledWith(expected);
+  });
+
+  it('should print a large even sized pyramid', () => {
+    const expected = `          XX
+         XXXX
+        XXXXXX
+       XXXXXXXX
+      XXXXXXXXXX
+     XXXXXXXXXXXX
+    XXXXXXXXXXXXXX
+   XXXXXXXXXXXXXXXX
+  XXXXXXXXXXXXXXXXXX
+ XXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXX`;
+
+    drill.printByArray(22);
+
+    expect(consoleLogSpy).toBeCalledWith(expected);
+  });
 });
