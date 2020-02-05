a8ad5b8300bedebe71d7d187cd7c25895454cf3a
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-02-04 21:10:23 -0800

    Added Server Favorites Solved

---
+++ 11-express/1-tuesday-servers/activities/05-Serve-Favorites/Solved/server-bonus.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Require dependencies
+const http = require('http');
+const fs = require('fs');
+
+// Set our port to 8080
+const PORT = 8080;
+
+const server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+  // Capture the url the request is made to
+  const path = req.url;
+
+  // When we visit different urls, call the function with different arguments
+  switch (path) {
+    case '/food':
+    case '/movies':
+    case '/modules':
+      return renderHTML(path + '.html', res);
+
+    default:
+      return renderHTML('/index.html', res);
+  }
+}
+
+// function to take a filepath and respond with html
+function renderHTML(filePath, res) {
+  return fs.readFile(__dirname + filePath, (err, data) => {
+    if (err) throw err;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+}
+
+// Starts our server.
+server.listen(PORT, () => console.log('Server is listening on PORT: ' + PORT));
