bb115fc50eb1289708311d3f320b0cfdcfd5763b
Spencer Shadley <spencershadley@utexas.edu>
2020-02-05 22:53:31 -0800

    added algorithms

---
+++ 12-mysql/algorithms/02-zeroes-and-ones/Unsolved/test/zeroes-and-ones.test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const expect = chai.expect;
+
+describe('zeroesAndOnes', function() {
+  it(`should return true when given '10101010'`, function() {
+    const str = '10101010';
+
+    const result = zeroesAndOnes(str);
+
+    expect(result).to.eql(true);
+  });
+
+  it(`should return false when given '00010'`, function() {
+    const str = '00010';
+
+    const result = zeroesAndOnes(str);
+
+    expect(result).to.eql(false);
+  });
+
+  it(`should return true when given the number
+  '001101'`, function() {
+    const str = '001101';
+
+    const result = zeroesAndOnes(str);
+
+    expect(result).to.eql(true);
+  });
+
+  it(`should return true when given ''`, function() {
+    const str = '';
+
+    const result = zeroesAndOnes(str);
+
+    expect(result).to.eql(true);
+  });
+});
---
+++ 12-mysql/algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Write code to create a function that accepts a string containing only 0s and 1s
+// Return true if there are an equal number of 0s and 1s
+// Else return false
+
+const zeroesAndOnes = function(str) {};
