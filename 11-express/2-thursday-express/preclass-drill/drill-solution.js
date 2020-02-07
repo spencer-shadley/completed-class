8e5a8a206944e3bc2978c60fde0aab592c750fc0
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-02-06 18:18:48 -0800

    Added Drill Solution

---
+++ 11-express/2-thursday-express/preclass-drill/drill-solution.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function printNoReverse(baseSize) {
+  let initialSideSpaces = baseSize / 2;
+  if (baseSize % 2 === 0) {
+    --initialSideSpaces;
+  }
+
+  let pyramid = '';
+  for (
+    let numSideSpaces = initialSideSpaces;
+    numSideSpaces >= 0;
+    --numSideSpaces
+  ) {
+    let side = ' '.repeat(numSideSpaces);
+    let x = 'X'.repeat(baseSize - side.length * 2);
+    pyramid += side + x;
+  }
+
+  console.log(pyramid);
+}
+
+function printPyramid(baseSize) {
+  let output = [];
+  let base = 'X'.repeat(baseSize);
+  let side = '';
+
+  while (base.length > 0) {
+    output.push(side + base);
+    side += ' ';
+    base = base.substring(2);
+  }
+
+  console.log(output.reverse().join('\n'));
+}
+
+module.exports = {
+  printNoReverse,
+  printPyramid
+};
