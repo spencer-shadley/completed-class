a384a7e5e82b37d9be7270b276de449e63f93fff
Spencer Shadley <spencershadley@utexas.edu>
2020-02-08 14:45:37 -0800

    in-class changes

--- 11-express/Week-11-Express/Express.md
+++ 11-express/Week-11-Express/Express.md
@@ -1,60 +1,62 @@
 ## <center> Testing / Express
 
-## Unit Testing 
+## Unit Testing
+
 ---
 
+- Unit testing is testing individual components of a feature/app.
 
-- Unit testing is testing individual components of a feature/app. 
-
-- Some times you will spend more time writing test code than actual code. 
+- Some times you will spend more time writing test code than actual code.
 
 - Consider use cases.
-    - How will this help accomplish my final product? 
+
+  - How will this help accomplish my final product?
 
 - Buddy Testing / Manual Testing
-    - Pull over another developer and explain feature to them. Have them test it out. 
-    - Second set of eyes. 
-    - ++RubberDuck
+  - Pull over another developer and explain feature to them. Have them test it out.
+  - Second set of eyes.
+  - ++RubberDuck
 - Keep tests simple and precise
-    - Each test should isolate a specific potential error.
-    - This can isolate specific errors very quickly.
-
+  - Each test should isolate a specific potential error.
+  - This can isolate specific errors very quickly.
 
 ## Function Testing
+
 ---
 
 1. What is the purpose of this function?
-    - Is this function necessary? What is it trying to accomplish?
+   - Is this function necessary? What is it trying to accomplish?
 1. Input
-    - Validate Input ( Type / Null / Undefined / Min / Max )
+   - Validate Input ( Type / Null / Undefined / Min / Max )
 1. Output
-    - Validate Output ( Type / Null / Undefined / Min / Max )
+   - Validate Output ( Type / Null / Undefined / Min / Max )
 1. Edge Cases
-    - Occurs at an extreme (maximum or minimum) operating parameter.
+   - Occurs at an extreme (maximum or minimum) operating parameter.
 1. Errors
-    - How should this function handle errors?
-    - Which corresponding errors should be thrown? 
+   - How should this function handle errors?
+   - Which corresponding errors should be thrown?
 
 ## <center> Real QA:
+
 ---
 
 ![QA-Bar](Images/QABar.jpg)
 
-
-
-
 ## Time Efficiency
+
 ---
 
 - You cannot posibly test for everything
-    - Remember your MVP (minimum viable product)
+  - Remember your MVP (minimum viable product)
 
 ![QA-Bar](Images/GoogleTesting.jpg)
 
-
 ## <center> Express
+
 ---
+
 Express is the most popular Node web framework, and is the underlying library for a number of other popular Node web frameworks. It can:
+
 1. Write handlers for requests with different HTTP verbs at different URL paths (routes).
 1. Integrate with "view" rendering engines in order to generate responses by inserting data into templates.
 1. Set common web application settings like the port to use for connecting, and the location of templates that are used for rendering the response.
@@ -63,6 +65,7 @@ Express is the most popular Node web framework, and is the underlying library fo
 ## Template
 
 ### Server.js
+
 ```js
 - const express = require('express');
 // Dependency
@@ -80,10 +83,10 @@ Express is the most popular Node web framework, and is the underlying library fo
 // express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object.
 
 - require('./route')(app);
-// Points the server to the route files. 
+// Points the server to the route files.
 
 - app.use('/static', express.static(path.join(__dirname, 'public')));
-   or 
+   or
 - app.use('/static', express.static('public'));
 // The above code to serve images, CSS files, and JavaScript files in a directory named public:
 
@@ -94,41 +97,40 @@ Express is the most popular Node web framework, and is the underlying library fo
 ```
 
 ### <center> CRUD
+
 ---
+
 - const app = express();
 
 - app.METHOD(PATH, HANDLER);
 
-1. Create - app.put('/', (req, res) => { res.send(`Hello World`) });
+1. Create - app.post('/', (req, res) => { res.send(`Hello World`) });
 1. Read - app.get();
 1. Update - app.put();
 1. Delete - app.delete(); or app.put();
 
 ### <center> Package.json
+
 ---
 
 - This file holds relevant metadata for the application.
-    - dependencies
-    - dev dependencies
-    - scripts
+  - dependencies
+  - dev dependencies
+  - scripts
 - `npm init`
 - `npm install --save` to install a dependency and update the package.json
 
 ### <center> .gitignore
+
 ---
 
-- You can specify to create a gitignore when you create a repository 
+- You can specify to create a gitignore when you create a repository
 - Create the file by right clicking and creating `.gitignore`:
 
 ```
 node_modules/
-.vscode 
+.vscode
 .idea/
 .DS_Store
 ~*.pptx
 ```
-
-
-
-
-
--- 11-express/Week-11-Express/script.js
+++ 11-express/Week-11-Express/script.js
@@ -1,88 +1,74 @@
 // Given a string, write a function to check if it is a permutation of a paldindrome.
-// A palindrome is a word or phrase that is the same forwards and backwards. 
-// A permutaiton is a rearrangement of letters. 
-// The palindrome does not need to be limited to just dictionary words. 
+// A palindrome is a word or phrase that is the same forwards and backwards.
+// A permutaiton is a rearrangement of letters.
+// The palindrome does not need to be limited to just dictionary words.
 // You can ignore casing and non-letter characters.
 
 // Example:
 // Input: Tact Coa
 // Output: true
 
-
 // I: string ([a-z][A-Z])
-// O: boolean 
+// O: boolean
 // Edge: str.length(min/max)?? numbers? Special characters?
 
-const palPerm = (str) => {
+const palPerm = str => {
+  // If string is even: 2 of every charcter
+  // If string is odd: 2 of every character with at most 1 odd
 
-    // If string is even: 2 of every charcter
-    // If string is odd: 2 of every character with at most 1 odd
+  // Hash map probably?
 
-    // Hash map probably?
-    
-    const hash = {};
-    let charCount = 0;
+  const hash = {};
+  let charCount = 0;
 
-    for (let i = 0; i < str.length; ++i) {
+  for (let i = 0; i < str.length; ++i) {
+    let c = str[i].toLowerCase();
 
-        let c = str[i].toLowerCase();
-
-        if (c === ' ') {
-            continue;
-        }
-        if (hash[c]) {
-            delete hash[c];
-        } else {
-            hash[c] = true;
-        }
-        charCount++;
+    if (c === ' ') {
+      continue;
     }
-
-    if (charCount % 2 === 0) {
-        return Object.keys(hash).length === 0;
+    if (hash[c]) {
+      delete hash[c];
     } else {
-        return Object.keys(hash).length === 1;
+      hash[c] = true;
     }
-}
+    charCount++;
+  }
 
-console.table([ 
-    ['Tacocat', palPerm('Tacocat') === true],
-    ['atco cat', palPerm('atco cat') === true],
-    [' rac ecar rara ', palPerm(' rac  ecar rara ') === true],
-    ['testfailed', palPerm('testfailed') === false],
-    ['clippers', palPerm('clippers') === false],
-    ['aabbc', palPerm('aabbc') === true],
-    ['aaaabbbbcc', palPerm('aaaabbbbcc') === true],
-    ['aabc', palPerm('aabc') === false],
-    ['   ', palPerm('   ') === true],
+  if (charCount % 2 === 0) {
+    return Object.keys(hash).length === 0;
+  } else {
+    return Object.keys(hash).length === 1;
+  }
+};
+
+console.table([
+  ['Tacocat', palPerm('Tacocat') === true],
+  ['atco cat', palPerm('atco cat') === true],
+  [' rac ecar rara ', palPerm(' rac  ecar rara ') === true],
+  ['testfailed', palPerm('testfailed') === false],
+  ['clippers', palPerm('clippers') === false],
+  ['aabbc', palPerm('aabbc') === true],
+  ['aaaabbbbcc', palPerm('aaaabbbbcc') === true],
+  ['aabc', palPerm('aabc') === false],
+  ['   ', palPerm('   ') === true]
 ]);
 
-// class testPerm {
-//     constructor(testInput, expectedResult, actualResult) {
-//         this.testInput = testInput;
-//         this.expectedResult = expectedResult;
-//         this.actualResult = actualResult;
-//     }
-// }
+class testPerm {
+  constructor(testInput, expectedResult, actualResult) {
+    this.testInput = testInput;
+    this.expectedResult = expectedResult;
+    this.actualResult = actualResult;
+  }
+}
 
-// let test1 = new testPerm('Tacocat', true, palPerm('Tacocat'));
-// let test2 = new testPerm('atco cat', true, palPerm('atco cat'));
-// let test3 = new testPerm(' rac  ecar rara', true, palPerm(' rac  ecar rara '));
-// let test4 = new testPerm('testfailed', false, palPerm('testfailed'));
-// let test5 = new testPerm('clippers', false, palPerm('clippers'));
-// let test6 = new testPerm('aabbc', true, palPerm('aabbc'));
-// let test7 = new testPerm('aabc', false, palPerm('aabc'));
-// let test8 = new testPerm('    ', true, palPerm('    '));
+let test1 = new testPerm('Tacocat', true, palPerm('Tacocat'));
+let test2 = new testPerm('atco cat', true, palPerm('atco cat'));
+let test3 = new testPerm(' rac  ecar rara', true, palPerm(' rac  ecar rara '));
+let test4 = new testPerm('testfailed', false, palPerm('testfailed'));
+let test5 = new testPerm('clippers', false, palPerm('clippers'));
+let test6 = new testPerm('aabbc', true, palPerm('aabbc'));
+let test7 = new testPerm('aabc', false, palPerm('aabc'));
+let test8 = new testPerm('    ', true, palPerm('    '));
 
-
-// console.table([
-//     test1,
-//     test2,
-//     test3,
-//     test4,
-//     test5,
-//     test6,
-//     test7,
-//     test8,
-// ]);
-
+console.table([test1, test2, test3, test4, test5, test6, test7, test8]);
