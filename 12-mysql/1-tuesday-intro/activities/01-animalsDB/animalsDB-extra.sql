24a3b429e7b07a0fe7956f150f61d26a1d4e4998
Spencer Shadley <spencershadley@utexas.edu>
2020-02-08 22:15:32 -0800

    split SQL DB into two parts

--- 12-mysql/1-tuesday-intro/activities/01-animalsDB/animalsDB-extra.sql
+++ 12-mysql/1-tuesday-intro/activities/01-animalsDB/animalsDB-extra.sql
@@ -17,3 +17,21 @@ CREATE TABLE people (
   -- Makes an numeric column called "pet_age" --
   pet_age INTEGER
 );
+
+-- Creates new rows containing data in all named columns --
+INSERT INTO people (name, has_pet, pet_name, pet_age)
+VALUES ("Ahmed", TRUE, "Rockington", 100);
+
+INSERT INTO people (name, has_pet, pet_name, pet_age)
+VALUES ("Ahmed", TRUE, "Rockington", 100);
+
+INSERT INTO people (name, has_pet, pet_name, pet_age)
+VALUES ("Jacob", TRUE, "Misty", 10);
+
+INSERT INTO people (name, has_pet)
+VALUES ("Peter", false);
+
+-- Updates the row where the column name is peter --
+UPDATE people
+SET has_pet = true, pet_name = "Franklin", pet_age = 2
+WHERE name = "Peter";
