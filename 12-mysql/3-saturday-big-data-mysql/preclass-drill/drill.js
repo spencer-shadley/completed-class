53724b628fc081c986ceac3954cc9eaec5a4e4ee
Spencer Shadley <spencershadley@utexas.edu>
2020-02-10 19:19:27 -0800

    added 12-3 drill

--- 12-mysql/3-saturday-big-data-mysql/preclass-drill/drill.js
+++ 12-mysql/3-saturday-big-data-mysql/preclass-drill/drill.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function hasSome(arr, callback) {}
+
+function hasNone(arr, callback) {}
+
+function hasAll(arr, callback) {}
+
+function hasMost(arr, callback) {}
+
+module.exports = {
+  hasSome,
+  hasAll,
+  hasNone,
+  hasMost
+};
