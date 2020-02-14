db9e78d88f295f95e790803a2bc014d8503b3bdb
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-02-13 19:25:09 -0800

    Added playlistRead Solved

---
+++ 12-mysql/2-thursday-mysql-node/activities/08-playlistRead/Solved/playlistRead.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Your password
+  password: '',
+  database: 'playlistDB'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log(`connected with id ${connection.threadId}`);
+  queryAllSongs();
+  queryDanceSongs();
+});
+
+function queryAllSongs() {
+  connection.query('SELECT * FROM songs', (err, songs) => {
+    if (err) throw err;
+    for (let song of songs) {
+      console.log(
+        `${song.id} | ${song.title} | ${song.artist} | ${song.genre}`
+      );
+    }
+    console.log('-----------------------------------');
+  });
+}
+
+function queryDanceSongs() {
+  const query = connection.query(
+    'SELECT * FROM songs WHERE genre=?',
+    ['Dance'],
+    (err, songs) => {
+      if (err) throw err;
+      for (let song of songs) {
+        console.log(
+          `${song.id} | ${song.title} | ${song.artist} | ${song.genre}`
+        );
+      }
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+  connection.end();
+}
