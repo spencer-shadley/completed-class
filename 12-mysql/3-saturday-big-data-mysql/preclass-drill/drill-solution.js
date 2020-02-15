1ed0370479dc1cdacce68270a2d7dabe27b8b160
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-02-15 09:59:19 -0800

    Added new drill-solution with JsDoc comments

--- 12-mysql/3-saturday-big-data-mysql/preclass-drill/drill-solution.js
+++ 12-mysql/3-saturday-big-data-mysql/preclass-drill/drill-solution.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const isValid = (arr, callback) => Array.isArray(arr) && !!callback;
+
+/**
+ * Checks if any of the elements in @param arr meet the condition @param callback
+ * @param {Array} arr The array to check against
+ * @param {Function} callback Condition which at least one element must meet
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some}
+ * @returns {boolean}
+ */
 function hasSome(arr, callback) {
-  if (!Array.isArray(arr) || !callback) {
+  if (!isValid(arr, callback)) {
     return false;
   }
 
@@ -13,12 +22,25 @@ function hasSome(arr, callback) {
   return false;
 }
 
+/**
+ * Checks if none of the elements in @param arr meet the condition @param callback
+ * @param {Array} arr The array to check against
+ * @param {Function} callback Condition which at least one element must meet
+ * @returns {boolean}
+ */
 function hasNone(arr, callback) {
   return !hasSome(arr, callback);
 }
 
+/**
+ * Checks if all of the elements in @param arr meet the condition @param callback
+ * @param {Array} arr The array to check against
+ * @param {Function} callback Condition all elements must meet
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every}
+ * @returns {boolean}
+ */
 function hasAll(arr, callback) {
-  if (!Array.isArray(arr) || !callback) {
+  if (!isValid(arr, callback)) {
     return false;
   }
 
@@ -30,8 +52,14 @@ function hasAll(arr, callback) {
   return true;
 }
 
+/**
+ * Checks if more than half of the elements in @param arr meet the condition @param callback
+ * @param {Array} arr The array to check against
+ * @param {Function} callback Condition which most elements must meet
+ * @returns {boolean}
+ */
 function hasMost(arr, callback) {
-  if (!Array.isArray(arr) || !callback) {
+  if (!isValid(arr, callback)) {
     return false;
   }
 
