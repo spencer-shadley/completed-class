955a0e1075dc41243dbd511f547903a68a13fd94
Spencer Shadley <spencershadley@utexas.edu>
2020-02-17 15:52:01 -0800

    rewrote solution

--- class-content/13-MVC/01-Activities/07-TaskSaver/server.js
+++ class-content/13-MVC/01-Activities/07-TaskSaver/server.js
@@ -38,25 +38,12 @@ connection.connect(err => {
 app.get('/', (req, res) => {
   connection.query('SELECT * FROM tasks;', (err, data) => {
     if (err) throw err;
-
-    // Test it
-    // console.log('The solution is: ', data);
-
-    // Test it
-    // return res.send(data);
-
     res.render('index', { tasks: data });
   });
 });
 
 // Post route -> back to home
 app.post('/', (req, res) => {
-  // Test it
-  // console.log('You sent, ' + req.body.task);
-
-  // Test it
-  // return res.send('You sent, ' + req.body.task);
-
   // When using the MySQL package, we'd use ?s in place of any values to be inserted, which are then swapped out with corresponding elements in the array
   // This helps us avoid an exploit known as SQL injection which we'd be open to if we used string concatenation
   // https://en.wikipedia.org/wiki/SQL_injection
