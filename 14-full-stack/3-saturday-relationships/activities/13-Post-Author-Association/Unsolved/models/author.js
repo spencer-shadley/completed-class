1baec8269ec16d2dabb2081c2ba62a93913ccc40
Spencer Shadley <spencershadley@utexas.edu>
2020-02-21 21:04:34 -0800

    added 14-3 activities

---
+++ 14-full-stack/3-saturday-relationships/activities/13-Post-Author-Association/Unsolved/models/author.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  const Author = sequelize.define('Author', {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING
+  });
+  return Author;
+};
