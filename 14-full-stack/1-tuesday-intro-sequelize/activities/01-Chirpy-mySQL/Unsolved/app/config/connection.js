4f755d7dcb8064382d1f6ea66b6c2588d39095bd
Spencer Shadley <spencershadley@utexas.edu>
2020-02-21 21:00:39 -0800

    added 14-1 activities

---
+++ 14-full-stack/1-tuesday-intro-sequelize/activities/01-Chirpy-mySQL/Unsolved/app/config/connection.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+// Require mysql
+const mysql = require('mysql');
+
+// Set up our connection information
+const connection = mysql.createConnection({
+  host: '',
+  port: 3306,
+  user: '',
+  password: '',
+  database: ''
+});
+
+// Connect to the database
+connection.connect(err => {
+  if (err) {
+    console.error('error connecting', err.stack);
+    return;
+  }
+  console.log(`connected with id ${connection.threadId}`);
+});
+
+// Export connection
+module.exports = connection;
