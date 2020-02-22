d6ec9940b5aa575cc8086c9f5bf204d5159623d0
Spencer Shadley <spencershadley@utexas.edu>
2020-02-21 19:54:37 -0800

    fixed bug

--- 14-full-stack/hw/Develop/models/index.js
+++ 14-full-stack/hw/Develop/models/index.js
@@ -8,16 +8,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-if (config.use_env_variable) {
-  const sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+let sequelize = config.use_env_variable
+  ? new Sequelize(process.env[config.use_env_variable])
+  : new Sequelize(config.database, config.username, config.password, config);
 
 fs.readdirSync(__dirname)
   .filter(function(file) {
