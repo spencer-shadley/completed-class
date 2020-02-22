cba80ce5fe748b00411fffe3d9b061c446329741
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-02-21 17:06:00 -0800

    Added a FINISHED version of the bonus MySQL review

---
+++ 12-mysql/Weekly-Review/MoreSql Finished/connectorClass.js
@@ -0,0 +1,58 @@
+// This is my javascript file. 
+// This is where I am going to write some sample Query Parameters to test.
+// Using MySQL workbench to test query parameters is a fantastic idea! 
+
+'use strict';
+
+const connection = require('./connection');
+
+class DB {
+    constructor(connection) {
+        this.connection = connection;
+    };
+
+
+    viewAllHouses() {
+        return this.connection.query(
+            `
+            SELECT 
+            house.id,
+            house.name
+            FROM 
+            house
+            `
+        );
+    };
+
+    viewAllRoles() {
+        return this.connection.query(
+            `
+                SELECT
+                role.id,
+                role.title,
+                role.salary,
+                house.name AS House
+                FROM
+                role
+                LEFT JOIN
+                house ON role.house_id = house.id
+                ORDER BY
+                role.id
+                `
+        );
+    };
+
+    createHouse(house) {
+        return this.connection.query(
+            `
+                INSERT INTO
+                    house
+                SET
+                    ?
+                `, house
+        );
+    };
+};
+
+
+module.export = new DB(connection);
