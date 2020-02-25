d63edad2e0900fdfc030fe483af3c05ad082a997
Spencer Shadley <spencershadley@utexas.edu>
2020-02-25 15:05:25 -0800

    added saturday drill

--- 14-full-stack/3-saturday-relationships/preclass-drill/drill.js
+++ 14-full-stack/3-saturday-relationships/preclass-drill/drill.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function haveSameTotalPosition(...words) {
+  if (!Array.isArray(words) || words.length < 2) {
+    return false;
+  }
+
+  const totalCount = total(words[0]);
+  words.splice(0, 1);
+
+  return words.every(word => totalCount === total(word));
+}
+
+function total(word) {
+  let count = 0;
+  for (const letter of word.toLowerCase()) {
+    const ascii = letter.charCodeAt(0);
+    if (ascii < 'a'.charCodeAt(0) || ascii > 'z'.charCodeAt(0)) {
+      return null;
+    }
+    count += ascii - 96;
+  }
+  return count;
+}
+
+module.exports = { haveSameTotalPosition };
--- 14-full-stack/3-saturday-relationships/preclass-drill/drill.md
+++ 14-full-stack/3-saturday-relationships/preclass-drill/drill.md
@@ -1,13 +1,25 @@
 # Pre-class Drill
 
----
+Take each letter in the alphabet and assign it a position (a=> 1, b => 2, etc.)
 
-Input
-
-Output
+Determine if the sum of all positions of characters in a word are equal, given any number of strings
 
 ---
 
 Input
 
+`abc`, `cab`
+
 Output
+
+`true`
+
+---
+
+Input
+
+`abc`, `f`
+
+Output
+
+`true`
