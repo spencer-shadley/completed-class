d4f0bd53053df55ad453efb9db554018e467c30c
Spencer Shadley <spencershadley@utexas.edu>
2020-02-24 18:26:14 -0600

    removed deprecated INTEGER size

--- class-content/14-Full-Stack/01-Activities/01-Chirpy-mySQL/Solved/schema.sql
+++ class-content/14-Full-Stack/01-Activities/01-Chirpy-mySQL/Solved/schema.sql
@@ -1,11 +1,13 @@
+DROP DATABASE IF EXISTS chirpy;
+
 CREATE DATABASE chirpy;
 USE chirpy;
 
 CREATE TABLE `chirps` (
-  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
   `author` VARCHAR( 255) NOT NULL,
   `body` VARCHAR( 255 ) NOT NULL,
   `created_at` DATETIME NOT NULL,
 
-  PRIMARY KEY ( `id` ) 
+  PRIMARY KEY (`id`)
 );
