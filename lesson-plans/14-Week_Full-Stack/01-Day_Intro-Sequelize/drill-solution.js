7b495de43fb30fb73a9951fd52393bc51a368025
Spencer Shadley <spencershadley@utexas.edu>
2020-02-25 14:31:42 -0800

    added drill solution; swapped drills

--- lesson-plans/14-Week_Full-Stack/01-Day_Intro-Sequelize/drill-solution.js
+++ lesson-plans/14-Week_Full-Stack/01-Day_Intro-Sequelize/drill-solution.js
@@ -1,39 +1,11 @@
 'use strict';
 
-function areAnagrams(...anagrams) {
-  if (!anagrams || anagrams.length < 2) {
+function haveSubWord(subword, ...words) {
+  if (!subword || !Array.isArray(words) || words.length === 0) {
     return false;
   }
 
-  const firstWordLetterCounts = countLetters(anagrams[0]);
-  anagrams.splice(0, 1);
-
-  return anagrams.every(anagram =>
-    hasMatchingLetterCounts(firstWordLetterCounts, anagram)
-  );
+  return words.every(word => word.indexOf(subword) !== -1);
 }
 
-function countLetters(word) {
-  const letterCounts = {};
-  for (const letter of word) {
-    const letterCount = letterCounts[letter];
-    if (letterCount) {
-      ++letterCounts[letter];
-    } else {
-      letterCounts[letter] = 1;
-    }
-  }
-  return letterCounts;
-}
-
-function hasMatchingLetterCounts(letterCounts, word) {
-  const wordLetterCounts = countLetters(word);
-  for (const key of Object.keys(letterCounts)) {
-    if (letterCounts[key] !== wordLetterCounts[key]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-module.exports = { areAnagrams };
+module.exports = { haveSubWord };
