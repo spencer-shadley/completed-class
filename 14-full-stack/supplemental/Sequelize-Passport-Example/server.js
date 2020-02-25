afe7104f95bda68315d03093bb05d395ccd48fdb
Spencer Shadley <spencershadley@utexas.edu>
2020-02-25 13:10:27 -0800

    removed parts of server

--- 14-full-stack/supplemental/Sequelize-Passport-Example/server.js
+++ 14-full-stack/supplemental/Sequelize-Passport-Example/server.js
@@ -1,13 +1,6 @@
 'use strict';
 
 const express = require('express');
-const db = require('./models');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`);
-  });
-});
