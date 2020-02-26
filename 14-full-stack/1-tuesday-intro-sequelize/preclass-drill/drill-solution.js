74522d1580a0167a4c828df95e0664251ee7363e
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-02-25 18:22:12 -0800

    Added correct PreClassDrill Solution

--- 14-full-stack/1-tuesday-intro-sequelize/preclass-drill/drill-solution.js
+++ 14-full-stack/1-tuesday-intro-sequelize/preclass-drill/drill-solution.js
@@ -1,58 +1,26 @@
 'use strict';
 
-function reverseNumber(number) {
-  if (Number.isNaN(number)) {
-    throw new Error(`${number} is not a number`);
+function haveSameTotalPosition(...words) {
+  if (!Array.isArray(words) || words.length < 2) {
+    return false;
   }
 
-  const isNegative = number < 0;
-  const numberAsString = number.toString();
-  let reversedNumberString = '';
-  for (let i = numberAsString.length - 1; i >= 0; --i) {
-    reversedNumberString += numberAsString.charAt(i);
-  }
+  const totalCount = total(words[0]);
+  words.splice(0, 1);
 
-  let reversedNumber = parseInt(reversedNumberString);
-
-  if (isNegative) {
-    reversedNumber *= -1;
-  }
-  return reversedNumber;
+  return words.every(word => totalCount === total(word));
 }
 
-function reverseNumberArr(number) {
-  if (Number.isNaN(number)) {
-    throw new Error(`${number} is not a number`);
+function total(word) {
+  let count = 0;
+  for (const letter of word.toLowerCase()) {
+    const ascii = letter.charCodeAt(0);
+    if (ascii < 'a'.charCodeAt(0) || ascii > 'z'.charCodeAt(0)) {
+      return null;
+    }
+    count += ascii - 96;
   }
-
-  const isNegative = number < 0;
-  const numberArr = number.toString().split('');
-  const reversedNumber =
-    parseInt(numberArr.reverse().join('')) * (isNegative ? -1 : 1);
-  return reversedNumber;
+  return count;
 }
 
-function reverseNumberDirect(number) {
-  if (Number.isNaN(number) || number === null || number === undefined) {
-    throw new Error(`${number} is not a number`);
-  }
-
-  const isNegative = number < 0;
-  number = Math.abs(number);
-
-  let reversedNumber = 0;
-  let lastDigit;
-  while (number > 0) {
-    lastDigit = number % 10;
-    reversedNumber = reversedNumber * 10 + lastDigit;
-    number = parseInt(number / 10);
-  }
-
-  return reversedNumber * (isNegative ? -1 : 1);
-}
-
-module.exports = {
-  reverseNumber: reverseNumberDirect
-  //   reverseNumber: reverseNumber
-  //   reverseNumber: reverseNumberArr
-};
+module.exports = { haveSameTotalPosition };
