2a9ce1b7731776f8f062750a9ce24a79f7041f18
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-02-29 12:56:35 -0800

    Added Solved Post-Author-Association

---
+++ 14-full-stack/3-saturday-relationships/activities/14-Post-Author-Association/Solved/routes/author-api-routes.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const db = require(`../models`);
+
+module.exports = app => {
+  // Find all Authors and return them to the user with res.json
+  app.get(`/api/authors`, (req, res) => {
+    db.Author.findAll({}).then(dbAuthor => {
+      res.json(dbAuthor);
+    });
+  });
+
+  app.get(`/api/authors/:id`, (req, res) => {
+    // Find one Author with the id in req.params.id and return them to the user with res.json
+    db.Author.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbAuthor => {
+      res.json(dbAuthor);
+    });
+  });
+
+  app.post(`/api/authors`, (req, res) => {
+    // Create an Author with the data available to us in req.body
+    console.log(req.body);
+    db.Author.create(req.body).then(dbAuthor => {
+      res.json(dbAuthor);
+    });
+  });
+
+  app.delete(`/api/authors/:id`, (req, res) => {
+    // Delete the Author with the id available to us in req.params.id
+    db.Author.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbAuthor => {
+      res.json(dbAuthor);
+    });
+  });
+};
