e882d7e3beb72b17258ecc91131bc35f8c34f5da
Spencer Shadley <spencershadley@utexas.edu>
2020-02-28 20:48:20 -0800

    updated sync comments

--- 14-full-stack/2-thursday-crud-sequelize/activities/08-Sequelize-Create-Read/Unsolved/server.js
+++ 14-full-stack/2-thursday-crud-sequelize/activities/08-Sequelize-Create-Read/Unsolved/server.js
@@ -26,6 +26,7 @@ app.use(express.static(`public`));
 // =============================================================
 require(`./routes/api-routes.js`)(app);
 
+// =============================================================
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync().then(() => {
--- 14-full-stack/3-saturday-relationships/activities/12-Blog-CRUD/Unsolved/server.js
+++ 14-full-stack/3-saturday-relationships/activities/12-Blog-CRUD/Unsolved/server.js
@@ -33,6 +33,7 @@ app.use(express.static(`public`));
 require(`./routes/api-routes.js`)(app);
 require(`./routes/html-routes.js`)(app);
 
+// =============================================================
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: isDebug }).then(() => {
