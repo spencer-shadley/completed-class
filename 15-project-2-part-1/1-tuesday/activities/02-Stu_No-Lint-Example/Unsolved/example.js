e01570362c0709e7b73c9edbd5e338b6a12b673f
Spencer Shadley <spencershadley@utexas.edu>
2020-02-29 22:51:09 -0800

    updated example

--- 15-project-2-part-1/1-tuesday/activities/02-Stu_No-Lint-Example/Unsolved/example.js
+++ 15-project-2-part-1/1-tuesday/activities/02-Stu_No-Lint-Example/Unsolved/example.js
@@ -2,14 +2,14 @@
 
 const num_1 = parseInt(prompt('Give me a number!'));
 const num_2 = parseInt(prompt('Give me another number!'));
-const result;
+var result;
 
 const operation = prompt(
   'What would you like to do? (add, subtract, multiply, divide)'
 ).toUpperCase();
 
 if (operation == 'ADD') {
-  const result = num_1 + num_2;
+  var result = num_1 + num_2;
   alert('The sum of ' + num_1 + ' and ' + num_2 + ' is ' + result);
 } else if (operation === 'SUBTRACT') {
   result = num_1 - num_2;
