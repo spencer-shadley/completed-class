1e3a69316943e9fc82eaba2ad8784dbfb8cffe50
Spencer Shadley <spencershadley@utexas.edu>
2020-03-01 23:32:40 -0800

    added drill

--- 15-project-2-part-1/3-saturday/preclass-drill/test/drill.test.js
+++ 15-project-2-part-1/3-saturday/preclass-drill/test/drill.test.js
@@ -2,6 +2,25 @@
 
 const drill = require('../drill');
 
-describe('', () => {
-  it('should', () => {});
+describe('findSumIndices', () => {
+  it('should find two indices', () => {
+    verify(3, [1, 2], [0, 1]);
+  });
+
+  it('should find two indices when there are multiple answers', () => {
+    verify(3, [0, 1, 2, 3, 4, 5], [0, 3]);
+  });
+
+  it('should not find when the target does not exist', () => {
+    verify(30, [0, 1, 2, 3, 4, 5], null);
+  });
+
+  it('should work with negatives', () => {
+    verify(2, [-3, 4, 5], [0, 2]);
+  });
+
+  function verify(target, nums, expected) {
+    const actual = drill.findSumIndices(target, nums);
+    expect(actual).toEqual(expected);
+  }
 });
