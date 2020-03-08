2cc51e5c4823d13308f593578aa91c392b256bcc
Spencer Shadley <spencershadley@utexas.edu>
2020-03-07 20:57:25 -0800

    rewrote hw solution

--- class-content/17-NoSQL/02-Homework/Develop/public/api.js
+++ class-content/17-NoSQL/02-Homework/Develop/public/api.js
@@ -18,7 +18,7 @@ const API = {
 
     const res = await fetch(`/api/workouts/${ id}`, {
       method: `PUT`,
-      headers: { "Content-Type": `application/json` },
+      headers: { 'Content-Type': `application/json` },
       body: JSON.stringify(data)
     });
 
@@ -30,7 +30,7 @@ const API = {
     const res = await fetch(`/api/workouts`, {
       method: `POST`,
       body: JSON.stringify(data),
-      headers: { "Content-Type": `application/json` }
+      headers: { 'Content-Type': `application/json` }
     });
 
     const json = await res.json();
