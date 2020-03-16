2d28a49717226fe696284a2382ba8c30f20bb768
Spencer Shadley <spencershadley@utexas.edu>
2020-03-15 21:32:15 -0700

    finalized activity

--- class-content/17-NoSQL/01-Activities/11-Stu-Mongoose-Schema/Solved/server.js
+++ class-content/17-NoSQL/01-Activities/11-Stu-Mongoose-Schema/Solved/server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const express = require(`express`);
-const logger = require(`morgan`);
 const mongoose = require(`mongoose`);
+const logger = require(`morgan`);
 
 const PORT = process.env.PORT || 3000;
 
