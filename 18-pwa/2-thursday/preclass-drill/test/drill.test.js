39ad55cf7df6b262e145da3f33ab3434cc209cb2
Spencer Shadley <spencershadley@utexas.edu>
2020-03-17 12:22:13 -0700

    added pwa template

---
+++ 18-pwa/2-thursday/preclass-drill/test/drill.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-empty-function */
+/* eslint-disable no-unused-vars */
+
+'use strict';
+
+const drill = require(`../drill`);
+
+describe(``, () => {
+  it(`should`, () => {});
+});
