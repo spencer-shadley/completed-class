bbee6882398c5189dfdaef2a51a993c02427b959
Spencer Shadley <spencershadley@utexas.edu>
2020-03-17 12:51:46 -0700

    moved to gitlab

--- class-content/18-PWA/01-Activities/11-Stu_Service_Workers/Unsolved/routes/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-module.exports = app => {
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-};
