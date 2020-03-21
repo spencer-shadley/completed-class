8dc3029babcfe8cfad53bc71275165425f2e248a
Spencer Shadley <spencershadley@utexas.edu>
2020-03-21 13:05:41 -0700

    updates

--- class-content/17-NoSQL/01-Activities/26-Stu-Mini-Project/Solved/server.js
+++ class-content/17-NoSQL/01-Activities/26-Stu-Mini-Project/Solved/server.js
@@ -14,7 +14,8 @@ app.use(express.static(`public`));
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
