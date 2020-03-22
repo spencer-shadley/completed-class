0108fc95d03fbb88ae1b7e1b08625dda2e6b47af
Spencer Shadley <spencershadley@utexas.edu>
2020-03-21 23:40:28 -0700

    many updates - this should be several commits

--- class-content/18-PWA/01-Activities/09-Stu_Manifest/Solved/server.js
+++ class-content/18-PWA/01-Activities/09-Stu_Manifest/Solved/server.js
@@ -4,7 +4,7 @@ const express = require(`express`);
 const mongoose = require(`mongoose`);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`public`));
 app.use(express.urlencoded({ extended: true }));
