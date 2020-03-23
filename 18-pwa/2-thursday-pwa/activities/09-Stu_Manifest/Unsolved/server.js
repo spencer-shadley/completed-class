709ec18b4dffc3bf92dd92080533f2628b71eee3
Spencer Shadley <spencershadley@utexas.edu>
2020-03-22 19:43:26 -0700

    made requires explicit and separate

--- 18-pwa/2-thursday-pwa/activities/09-Stu_Manifest/Unsolved/server.js
+++ 18-pwa/2-thursday-pwa/activities/09-Stu_Manifest/Unsolved/server.js
@@ -3,6 +3,9 @@
 const express = require(`express`);
 const mongoose = require(`mongoose`);
 
+const apiRoutes = require(`./routes/apiRoutes`);
+const htmlRoutes = require(`./routes/htmlRoutes`);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,8 +13,8 @@ app.use(express.static(`public`));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require(`./routes/apiRoutes`)(app);
-require(`./routes/htmlRoutes`)(app);
+apiRoutes(app);
+htmlRoutes(app);
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/imageperformance`, {
   useNewUrlParser: true,
@@ -20,6 +23,4 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/imageperformanc
   useFindAndModify: false
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Now listening on port: http://localhost:${PORT}`));
