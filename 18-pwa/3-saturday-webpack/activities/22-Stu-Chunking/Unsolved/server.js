0904bd9d83121c02dad1ebc739f46a2a1d601c70
Spencer Shadley <spencershadley@utexas.edu>
2020-03-22 19:50:43 -0700

    separated routes to their own require

--- 18-pwa/3-saturday-webpack/activities/22-Stu-Chunking/Unsolved/server.js
+++ 18-pwa/3-saturday-webpack/activities/22-Stu-Chunking/Unsolved/server.js
@@ -3,6 +3,9 @@
 const express = require(`express`);
 const mongoose = require(`mongoose`);
 
+const apiRoutes = require(`./routes/apiRoutes`);
+const htmlRoutes = require(`./routes/htmlRoutes`);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,8 +13,8 @@ app.use(express.static(`public`));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require(`./routes/apiRoutes`)(app);
-require(`./routes/htmlRoutes`)(app);
+apiRoutes(app);
+htmlRoutes(app);
 
 mongoose.connect(
   process.env.MONGODB_URI || `mongodb://localhost/imageperformance`,
