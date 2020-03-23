9ea573ba03ef1c321b4952e67fb38d50799eb79c
Spencer Shadley <spencershadley@utexas.edu>
2020-03-22 21:12:57 -0700

    improved names; fixed eslint

--- class-content/18-PWA/01-Activities/13-Stu_Caching_Fetching_Files/Solved/public/service-worker.js
+++ class-content/18-PWA/01-Activities/13-Stu_Caching_Fetching_Files/Solved/public/service-worker.js
@@ -59,8 +59,8 @@ const CACHE_NAME = `static-cache-v2`;
 const DATA_CACHE_NAME = `data-cache-v1`;
 
 // install
-self.addEventListener(`install`, evt => {
-  evt.waitUntil(
+self.addEventListener(`install`, event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log(`Your files were pre-cached successfully!`);
       return cache.addAll(FILES_TO_CACHE);
@@ -79,6 +79,7 @@ self.addEventListener(`activate`, evt => {
           console.log(`Removing old cache data`, key);
           return caches.delete(key);
         }
+        return undefined;
       })
     ))
   );
@@ -99,9 +100,11 @@ self.addEventListener(`fetch`, evt => {
 
           return response;
         })
-        .catch(err =>
-        // Network request failed, try to get it from the cache.
-          cache.match(evt.request)
+        .catch(err => {
+          // Network request failed, try to get it from the cache.
+          cache.match(evt.request);
+          console.error(err);
+        }
         )).catch(err => console.log(err))
     );
 
