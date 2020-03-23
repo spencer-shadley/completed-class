86da1734f83ea4a349025484c432febfda0d9a19
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-03-23 11:22:36 -0700

    Added Solved Week 17 Algorithms

---
+++ 17-NoSQL/algorithms/02-min-increment-for-unique/Solved/min-increment-for-unique.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+
+'use strict';
+
+// Write a function to take an array of integers and return
+// the minimum number of increments required across the array to ensure that every number is unique
+
+const minIncrement = nums => {
+  const uniqueNums = {};
+  let increments = 0;
+
+  for (let i = 0; i < nums.length; ++i) {
+    while (uniqueNums[nums[i]]) {
+      nums[i] += 1;
+      increments++;
+    }
+
+    uniqueNums[nums[i]] = true;
+  }
+
+  return increments;
+};
---
+++ 17-NoSQL/algorithms/03-peak-finder/Solved/test/peak-finder.test.js
@@ -0,0 +1,46 @@
+'use strict';
+/* eslint-disable no-magic-numbers */
+
+const expect = chai.expect;
+
+describe(`peakFinder`, () => {
+  it(`should return 3 if given the array [3, 2, 1]`, () => {
+    const arr = [3, 2, 1];
+
+    const result = peakFinder(arr);
+
+    expect(result).to.eql(3);
+  });
+
+  it(`should return 3 if given the array [2, 4, 6, 8, 10]`, () => {
+    const arr = [2, 4, 6, 8, 10];
+
+    const result = peakFinder(arr);
+
+    expect(result).to.eql(10);
+  });
+
+  it(`should return 3 if given the array [4]`, () => {
+    const arr = [4];
+
+    const result = peakFinder(arr);
+
+    expect(result).to.eql(4);
+  });
+
+  it(`should return 13 if given the array [-3, 2, 4, 7, 9, 13, 11, 8, 7, 3, 1]`, () => {
+    const arr = [-3, 2, 4, 7, 9, 13, 11, 8, 7, 3, 1];
+
+    const result = peakFinder(arr);
+
+    expect(result).to.eql(13);
+  });
+
+  it(`should return 1 if given the array [-3, -2, -1, 0, 1, -9, -11, -18, -70]`, () => {
+    const arr = [-3, -2, -1, 0, 1, -9, -11, -18, -70];
+
+    const result = peakFinder(arr);
+
+    expect(result).to.eql(1);
+  });
+});
