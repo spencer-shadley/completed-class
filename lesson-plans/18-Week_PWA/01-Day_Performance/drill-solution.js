726adb8651e3103325a92607eeba3274557e59dd
Spencer Shadley <spencershadley@utexas.edu>
2020-03-23 23:30:47 -0700

    added 18.1 drill solution

--- lesson-plans/18-Week_PWA/01-Day_Performance/drill-solution.js
+++ lesson-plans/18-Week_PWA/01-Day_Performance/drill-solution.js
@@ -1,30 +1,113 @@
-'use strict'
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
 
-// TODO: in-progress
-// https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/javascript
+/**
+ * Problem from codewars - https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+ * 
+ * @description
+ * convert a string to a new string where each character in the new string is "("
+ * if that character appears only once in the original string,
+ * or ")" if that character appears more than once in the original string.
+ * 
+ * Ignore capitalization when determining if a character is a duplicate.
+ * 
+ * @example recede => ()()()
+ * @param {String} word 
+ * @returns {String}
+ * 
+ * @summary
+ * This takes two passes to efficiently solve. The first pass counts the letters so that
+ * we can easily know if a letter is duplicate in the second pass.
+ * 
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+function duplicateEncode(word){
+  const letterCounts = {}; // O(N) space
 
-function formatDuration(seconds) {
-  const mapping = [
-    { 'seconds': 60 },
-    { 'minutes': 60 },
-    { 'hours': 24 },
-    { 'days': 365 },
-    { 'years': 1 }
-  ];
-
-  for (const map of mapping) {
-    
+  for (const letter of word) { // O(N) time
+    if (letterCounts[letter]) {
+      ++letterCounts[letter];
+    } else {
+      letterCounts[letter] = 1;
+    }
   }
-
-  let seconds = seconds % 60;
-  let minutes = seconds / 60;
-  let hours = minutes % 60;
   
-  let remainder = minutes / 60;
-  
-  let days = remainder % 24;
-  
-  console.log(days);
+  let output = '';
+  for(const letter of word) { // O(N) time
+    output += letterCounts[letter] === 1 ? '(' : ')';
+  }
+  return output;
 }
 
-formatDuration(62);
+/**
+ * Problem from codewars - https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+ * 
+ * @description
+ * convert a string to a new string where each character in the new string is "("
+ * if that character appears only once in the original string,
+ * or ")" if that character appears more than once in the original string.
+ * 
+ * Ignore capitalization when determining if a character is a duplicate.
+ * 
+ * @example recede => ()()()
+ * @param {String} word 
+ * @returns {String}
+ * 
+ * @summary
+ * This takes two passes to efficiently solve. The first pass counts the letters so that
+ * we can easily know if a letter is duplicate in the second pass.
+ * 
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+function duplicateEncode(word){
+  const letterCounts = {}; // O(N) space
+
+  const lowerCasedWord = word.toLowerCase(); // O(N) time and space
+  for (const letter of lowerCasedWord) { // O(N) time
+    if (letterCounts[letter]) {
+      ++letterCounts[letter];
+    } else {
+      letterCounts[letter] = 1;
+    }
+  }
+  
+  let output = '';
+  for(const letter of lowerCasedWord) { // O(N) time
+    output += letterCounts[letter] === 1 ? '(' : ')';
+  }
+  return output;
+}
+
+/**
+ * Problem from codewars - https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+ * 
+ * @description
+ * convert a string to a new string where each character in the new string is "("
+ * if that character appears only once in the original string,
+ * or ")" if that character appears more than once in the original string.
+ * 
+ * Ignore capitalization when determining if a character is a duplicate.
+ * 
+ * @example recede => ()()()
+ * @param {String} word 
+ * @returns {String}
+ * 
+ * @summary
+ * This takes two passes to efficiently solve. The first pass counts the letters so that
+ * we can easily know if a letter is duplicate in the second pass.
+ * 
+ * Time complexity: O(N^2)
+ * Space complexity: O(N)
+ */
+function duplicateEncodeSlow(word) {
+  let output = '';
+  const lowerCasedWord = word.toLowerCase(); // O(N) time and space
+  for (const letter of lowerCasedWord) { // O(N) time
+    const isRepeatingLetter = lowerCasedWord.indexOf(letter) === lowerCasedWord.lastIndexOf(letter); // O(N)
+    output += isRepeatingLetter ? '(' : ')';
+  }
+  return output;
+}
+
+duplicateEncode('recede');
