d4ac387eb57c9562923771ccbe364b5f4aa57677
Spencer Shadley <spencershadley@utexas.edu>
2020-03-24 00:33:31 -0700

    cleaned up api routes; added http-status-codes

--- class-content/18-PWA/02-Homework/Solution/routes/api.js
+++ class-content/18-PWA/02-Homework/Solution/routes/api.js
@@ -1,37 +1,25 @@
-'use strict';
-
 const router = require(`express`).Router();
+const HttpStatus = require(`http-status-code`);
+
 const Transaction = require(`../models/transaction.js`);
 
 router.post(`/api/transaction`, ({ body }, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+    .then(dbTransaction => res.json(dbTransaction))
+    .catch(err => res.status(HttpStatus.NOT_FOUND).json(err));
 });
 
 router.post(`/api/transaction/bulk`, ({ body }, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+    .then(dbTransaction => res.json(dbTransaction))
+    .catch(err => res.status(HttpStatus.NOT_FOUND).json(err));
 });
 
 router.get(`/api/transaction`, (req, res) => {
   Transaction.find({})
     .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+    .then(dbTransaction => res.json(dbTransaction))
+    .catch(err => res.status(HttpStatus.NOT_FOUND).json(err));
 });
 
 module.exports = router;
