b3fb75c65f0d7dfbcc74290a76a2f9e416da10bc
Spencer Shadley <spencershadley@utexas.edu>
2020-03-26 00:59:57 -0700

    added class component

--- class-content/19-React/01-Activities/03-Stu_HelloDiv/Solved/src/components/HelloDiv.js
+++ class-content/19-React/01-Activities/03-Stu_HelloDiv/Solved/src/components/HelloDiv.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
-function HelloDiv() {
-  return (
-    <div>
-      <h1>Hey guys! I'm a header!</h1>
-      <h2>Things I like!</h2>
-      <ul>
-        <li>Peas</li>
-        <li>Carrots</li>
-        <li>Love</li>
-      </ul>
-    </div>
-  );
+class HelloDiv extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>Hey guys! I'm a header!</h1>
+          <h2>Things I like!</h2>
+          <ul>
+            <li>Peas</li>
+            <li>Carrots</li>
+          <li>Love</li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+// functional component
+// function HelloDiv() {
+//   return (
+//     <div>
+//       <h1>Hey guys! I'm a header!</h1>
+//       <h2>Things I like!</h2>
+//       <ul>
+//         <li>Peas</li>
+//         <li>Carrots</li>
+//         <li>Love</li>
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default HelloDiv;
