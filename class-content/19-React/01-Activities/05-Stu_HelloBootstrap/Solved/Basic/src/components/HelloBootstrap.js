2bb4916ed9dcc4fb604a4d30d0811199431b4a6c
Spencer Shadley <spencershadley@utexas.edu>
2020-03-26 00:11:09 -0700

    made href=# look like a normal link

--- class-content/19-React/01-Activities/05-Stu_HelloBootstrap/Solved/Basic/src/components/HelloBootstrap.js
+++ class-content/19-React/01-Activities/05-Stu_HelloBootstrap/Solved/Basic/src/components/HelloBootstrap.js
@@ -4,7 +4,7 @@ function HelloBootstrap() {
   return (
     <div className="container">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="# ">
           Navbar
         </a>
         <button
@@ -21,19 +21,19 @@ function HelloBootstrap() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="# ">
                 Home <span className="sr-only">(current)</span>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="# ">
                 Link
               </a>
             </li>
             <li className="nav-item dropdown">
               <a
                 className="nav-link dropdown-toggle"
-                href="#"
+                href="# "
                 id="navbarDropdown"
                 role="button"
                 data-toggle="dropdown"
@@ -43,20 +43,20 @@ function HelloBootstrap() {
                 Dropdown
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="# ">
                   Action
                 </a>
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="# ">
                   Another action
                 </a>
                 <div className="dropdown-divider" />
-                <a className="dropdown-item" href="#">
+                <a className="dropdown-item" href="# ">
                   Something else here
                 </a>
               </div>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#">
+              <a className="nav-link disabled" href="# ">
                 Disabled
               </a>
             </li>
--- class-content/19-React/01-Activities/05-Stu_HelloBootstrap/Solved/Bonus/src/components/Navbar.js
+++ class-content/19-React/01-Activities/05-Stu_HelloBootstrap/Solved/Bonus/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="# ">
         Navbar
       </a>
       <button
@@ -20,19 +20,19 @@ function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="# ">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="# ">
               Link
             </a>
           </li>
           <li className="nav-item dropdown">
             <a
               className="nav-link dropdown-toggle"
-              href="#"
+              href="# "
               id="navbarDropdown"
               role="button"
               data-toggle="dropdown"
@@ -42,20 +42,20 @@ function Navbar() {
               Dropdown
             </a>
             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="#">
+              <a className="dropdown-item" href="# ">
                 Action
               </a>
-              <a className="dropdown-item" href="#">
+              <a className="dropdown-item" href="# ">
                 Another action
               </a>
               <div className="dropdown-divider" />
-              <a className="dropdown-item" href="#">
+              <a className="dropdown-item" href="# ">
                 Something else here
               </a>
             </div>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#">
+            <a className="nav-link disabled" href="# ">
               Disabled
             </a>
           </li>
