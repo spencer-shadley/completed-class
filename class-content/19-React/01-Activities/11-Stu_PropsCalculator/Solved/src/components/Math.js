e055834a905c42c27b161f5afae1d69158573a0b
Spencer Shadley <spencershadley@utexas.edu>
2020-03-27 16:19:46 -0700

    rewrote activity 11

--- class-content/19-React/01-Activities/11-Stu_PropsCalculator/Solved/src/components/Math.js
+++ class-content/19-React/01-Activities/11-Stu_PropsCalculator/Solved/src/components/Math.js
@@ -1,30 +1,38 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 // The Math function component accepts a props argument
 function Math(props) {
-  let value;
+  let result;
 
   // Assign value based on the operator
   switch (props.operator) {
     case '+':
-      value = props.num1 + props.num2;
+      result = props.num1 + props.num2;
       break;
     case '-':
-      value = props.num1 - props.num2;
+      result = props.num1 - props.num2;
       break;
     case '*':
-      value = props.num1 * props.num2;
+      result = props.num1 * props.num2;
       break;
     case '/':
-      value = props.num1 / props.num2;
+      result = props.num1 / props.num2;
       break;
     default:
-      value = NaN;
+      result = NaN;
   }
 
   // Return a span element containing the calculated value
   // Set the fontSize to the value in pixels
-  return <span style={{ fontSize: value }}>{value}</span>;
+  return <span style={{ fontSize: result }}>{result}</span>;
 }
+
+Math.propTypes = {
+  children: PropTypes.node,
+  num1: PropTypes.number,
+  num2: PropTypes.number,
+  operator: PropTypes.string
+};
 
 export default Math;
