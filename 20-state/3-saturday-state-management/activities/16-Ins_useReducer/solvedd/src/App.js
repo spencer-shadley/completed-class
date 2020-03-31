6076b593651bbf0809e553f0646232930766d663
Spencer Shadley <spencershadley@utexas.edu>
2020-03-30 23:15:29 -0700

    removed solved versions of redux

--- 20-state/3-saturday-state-management/activities/16-Ins_useReducer/solvedd/src/App.js
+++ 20-state/3-saturday-state-management/activities/16-Ins_useReducer/solvedd/src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  const dispatch = useDispatch();
   return (
       <div className="App">
-        <h1>{useSelector(state => state)}</h1>
-        <button onClick={() => dispatch({type: 'INCREMENT'})}>+</button>
-        <button onClick={() => dispatch({type: 'DECREMENT'})}>-</button>
       </div>
   );
 }
--- 20-state/3-saturday-state-management/activities/16-Ins_useReducer/solvedd/src/index.js
+++ 20-state/3-saturday-state-management/activities/16-Ins_useReducer/solvedd/src/index.js
@@ -2,27 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-function counter(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-let store = createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
     <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
