1e04e97f36bc8749c24b8438b27fa20a4f5ef118
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-03-31 20:11:38 -0700

    Added solved 03-Stu_HelloDiv

---
+++ 19-react/1-tuesday-intro-react/activities/03-Stu_HelloDiv/Solved/src/components/HelloDiv.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class HelloDiv extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>Hey guys! I'm a header!</h1>
+          <h2>Things I like!</h2>
+          <ul>
+            <li>Peas</li>
+            <li>Carrots</li>
+          <li>Love</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default HelloDiv;
