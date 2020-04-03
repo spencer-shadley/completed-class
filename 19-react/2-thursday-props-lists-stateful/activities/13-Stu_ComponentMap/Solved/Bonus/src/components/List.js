9a29bb2402bd9a6dedc5c0b17ea4e4f2872e85c5
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-04-02 20:02:24 -0700

    Added solved 13-Stu_ComponentMap

---
+++ 19-react/2-thursday-props-lists-stateful/activities/13-Stu_ComponentMap/Solved/Bonus/src/components/List.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+// Whenever we try to render an array containing JSX, React knows to render each JSX element separately
+function List(props) {
+  // Using the filter method, we can create a new array containing only groceries which haven't been purchased
+  const notPurchased = props.groceries.filter(grocery => !grocery.purchased);
+
+  return (
+    <ul className="list-group">
+      {notPurchased.map(item => (
+        <li className="list-group-item" key={item.id}>
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+List.propTyes = {
+  groceries: PropTypes.array
+}
+
+export default List;
