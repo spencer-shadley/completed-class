df784cdb2195304c7565fb8168744d7f0288505c
Spencer Shadley <spencershadley@utexas.edu>
2020-04-03 23:36:21 -0700

    style fixes to drill

--- lesson-plans/19-Week_React/02-Day_Props-Lists-Stateful/drill-solution.js
+++ lesson-plans/19-Week_React/02-Day_Props-Lists-Stateful/drill-solution.js
@@ -3,64 +3,64 @@
 /**
  * M = total of arrays length
  * N = total of target length
- * 
+ *
  * Note - the below is basically OXlogX) for each sort in practicality but is technically O(X^2)
- * 
+ *
  * Combine (concat) the arrays (time: O(M))
  * Sort the combined array (time: O(MlogM))
  * Convert the sorted combined array to a string for easy equality comparison (time: O(M), space: O(M))
  * Sort the target (time: O(NlogN))
  * Convert the target to a string for easy equality comparison (time: O(N), space: O(N))
  * Check if the combined sorted array (as a string) equals the sorted target array (as a string) (time: O(M + N))
- * 
+ *
  * time: O(MlogM + NlogN)
  * space: O(M + N)
- * 
- * @param {Array} arrays 
- * @param {Array} target 
+ *
+ * @param {Array} arrays
+ * @param {Array} target
  */
 function canConcatenateCondensed(arrays, target) {
-    return ([]
+    return []
         .concat(...arrays)
         .sort()
-        .join('')
+        .join(``)
         ===
         target
             .sort()
-            .join(''));
+            .join(``);
 }
 
 /**
  * K-Way MergeSort
- * 
+ *
  * Sort the arrays so that they are easy to compare
- * 
+ *
  * Find the maximum value from the set of strings (which will be the last index of one of the arrays)
- * 
+ *
  * Push that maximum value onto a merged array
- * 
+ *
  * Check that the merged array (which is sorted) equals the sorted target
- * 
- * @param {Array} arrays 
- * @param {Array} target 
- * 
+ *
+ * @param {Array} arrays
+ * @param {Array} target
+ *
  * M = total of arrays length
  * N = total of target length
- * 
+ *
  * Time complexity: O(MlogM + NlogN)
  * Space complexity: O(M)
- * 
+ *
  * About Merge Sort:
- * 
+ *
  * Uses a divide and conquer strategy - divide the problem into smaller sorted collections
  * and then combine those smaller collection
- * 
+ *
  * Advantages of sorting with MergeSort
  * - Scales well, supports sorting massive datasets, supporting external sorting and parallelization
  * - Fast, even in the worst case (ONlogN)
  * - Stable, elements are sorted in a predictable order, even when their values match
  * - Supports many different collection types
- * 
+ *
  * Disadvantages of sorting with MergeSort
  * - Not in-place, meaning it will take additional memory to sort because it keeps intermidate arrays
  * - Low cache locality hits
@@ -77,8 +77,8 @@ function canConcatenate(arrays, target) {
 
 /**
  * Checks if the given arrays are equal
- * @param {Array} arrOne 
- * @param {Array} arrTwo 
+ * @param {Array} arrOne
+ * @param {Array} arrTwo
  */
 function areEqual(arrOne, arrTwo) {
     for (let i = 0; i < arrOne.length; ++i) {
@@ -91,7 +91,7 @@ function areEqual(arrOne, arrTwo) {
 
 /**
  * Sorts the given arrays in-place in ascending order
- * @param {Array} arrays 
+ * @param {Array} arrays
  */
 function sortArrays(arrays) {
     for (const arr of arrays) {
@@ -102,9 +102,9 @@ function sortArrays(arrays) {
 /**
  * K-Way Merge, combining all arrays
  * https://en.wikipedia.org/wiki/K-way_merge_algorithm
- * 
+ *
  * @requires arrays to be sorted in ascending order
- * @param {Array} arrays 
+ * @param {Array} arrays
  */
 function kMerge(arrays) {
     const merged = []; // space: O(M)
@@ -116,8 +116,8 @@ function kMerge(arrays) {
         let maxIndex = 0;
 
         for (let i = 0; i < arrays.length; ++i) {
-            let currArr = arrays[i];
-            let currArrMax = currArr[currArr.length - 1];
+            const currArr = arrays[i];
+            const currArrMax = currArr[currArr.length - 1];
             if (currArrMax > maxValue || maxValue === undefined) {
                 maxValue = currArrMax;
                 maxIndex = i;
@@ -129,14 +129,14 @@ function kMerge(arrays) {
     return merged;
 }
 
-console.log(canConcatenate([[1, 2, 3, 4], [5, 6], [7]], [1, 2, 3, 4, 5, 6, 7]) === true)
-console.log(canConcatenate([[2, 1, 3], [5, 4, 7, 6]], [1, 2, 3, 4, 5, 6, 7]) === true)
-console.log(canConcatenate([[2, 1, 3], [5, 4, 7, 6]], [7, 6, 5, 4, 3, 2, 1]) === true)
-console.log(canConcatenate([[2, 1, 3], [5, 4, 7, 6, 7]], [1, 2, 3, 4, 5, 6, 7]) === false)
-console.log(canConcatenate([[2, 1, 3], [5, 4, 7]], [1, 2, 3, 4, 5, 6, 7]) === false)
-console.log(canConcatenate([[1, 4], [3]], [1, 3, 4]) === true)
-console.log(canConcatenate([[1, 4], [3]], [1, 2, 3, 4]) === false)
-console.log(canConcatenate([[1, 4], [3]], [4, 3, 1]) === true)
-console.log(canConcatenate([[1, 4], [2, 3]], [4, 3, 1, 2]) === true)
-console.log(canConcatenate([[1], [2], [3, 4]], [4, 3, 1, 2]) === true)
-console.log(canConcatenate([[1], [2], [3], [4]], [4, 3, 1, 2]) === true)
+console.log(canConcatenate([[1, 2, 3, 4], [5, 6], [7]], [1, 2, 3, 4, 5, 6, 7]) === true);
+console.log(canConcatenate([[2, 1, 3], [5, 4, 7, 6]], [1, 2, 3, 4, 5, 6, 7]) === true);
+console.log(canConcatenate([[2, 1, 3], [5, 4, 7, 6]], [7, 6, 5, 4, 3, 2, 1]) === true);
+console.log(canConcatenate([[2, 1, 3], [5, 4, 7, 6, 7]], [1, 2, 3, 4, 5, 6, 7]) === false);
+console.log(canConcatenate([[2, 1, 3], [5, 4, 7]], [1, 2, 3, 4, 5, 6, 7]) === false);
+console.log(canConcatenate([[1, 4], [3]], [1, 3, 4]) === true);
+console.log(canConcatenate([[1, 4], [3]], [1, 2, 3, 4]) === false);
+console.log(canConcatenate([[1, 4], [3]], [4, 3, 1]) === true);
+console.log(canConcatenate([[1, 4], [2, 3]], [4, 3, 1, 2]) === true);
+console.log(canConcatenate([[1], [2], [3, 4]], [4, 3, 1, 2]) === true);
+console.log(canConcatenate([[1], [2], [3], [4]], [4, 3, 1, 2]) === true);
