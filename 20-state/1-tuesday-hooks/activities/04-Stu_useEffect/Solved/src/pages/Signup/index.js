a93da61435194d815620c9f33e638a04f6ab8785
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-04-07 20:12:16 -0700

    Added solved 04-Stu_useEffect

--- 20-state/1-tuesday-hooks/activities/04-Stu_useEffect/Solved/src/pages/Signup/index.js
+++ 20-state/1-tuesday-hooks/activities/04-Stu_useEffect/Solved/src/pages/Signup/index.js
@@ -3,7 +3,7 @@ import Container from '../../components/Container';
 import Col from '../../components/Col';
 import Row from '../../components/Row';
 
-function Signup() {
+const Signup = () => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
@@ -16,7 +16,7 @@ function Signup() {
   return (
     <div>
       <div className="mt-4">
-        <h2>Sign Up</h2>
+        <h2>Welcome to Wikipedia Searcher!</h2>
       </div>
       <form onSubmit={handleSubmit}>
         <Container className="mt-3 px-5">
@@ -55,6 +55,6 @@ function Signup() {
       </form>
     </div>
   );
-}
+};
 
 export default Signup;
