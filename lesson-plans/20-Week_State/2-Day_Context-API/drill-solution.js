0dfcf81c74699df6ef181b5f363128bdddce4f25
Spencer Shadley <spencershadley@utexas.edu>
2020-04-09 00:43:51 -0700

    deleted part of solution

--- lesson-plans/20-Week_State/2-Day_Context-API/drill-solution.js
+++ lesson-plans/20-Week_State/2-Day_Context-API/drill-solution.js
@@ -24,46 +24,6 @@ function candies(numScores, scores) {
     return candies.reduce((currTotal, candy) => currTotal + candy, 0);
 }
 
-// function candies(numScores, scores) {
-//     return Math.min(minFromLeft(scores), minFromRight(scores));
-// }
-
-// function minFromLeft(scores) {
-//     let previousScore = scores[0];
-//     let prevCandyAmount = scores[0] < scores[1] ? 1 : 2;
-//     let totalCandy = prevCandyAmount;
-
-//     for (let i = 1; i < scores.length; ++i) {
-//         const score = scores[i];
-//         const isAscending = previousScore < score;
-//         const candyAmount = isAscending ? prevCandyAmount + 1 : 1;
-
-//         totalCandy += candyAmount;
-//         prevCandyAmount = candyAmount;
-//         previousScore = score;
-//     }
-
-//     return totalCandy;
-// }
-
-// function minFromRight(scores) {
-//     let previousScore = scores[scores.length - 1];
-//     let prevCandyAmount = scores[scores.length - 1] > scores[scores.length - 1] ? 2 : 1;
-//     let totalCandy = prevCandyAmount;
-
-//     for (let i = scores.length - 1; i >= 0; --i) {
-//         const score = scores[i];
-//         const isDescending = previousScore >= score;
-//         const candyAmount = isDescending ? 1 : prevCandyAmount + 1;
-
-//         totalCandy += candyAmount;
-//         prevCandyAmount = candyAmount;
-//         previousScore = score;
-//     }
-
-//     return totalCandy;
-// }
-
 // bonus - https://photos.google.com/photo/AF1QipPJskjLawlKndJVHlVNpQLoz04ED-omnYJw_LRf
 
 // 1. sort by score
