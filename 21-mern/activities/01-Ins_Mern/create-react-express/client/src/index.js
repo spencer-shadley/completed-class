167f06eeecad95f353a3edf8d895e8fb8b2bf523
Spencer Shadley <spencershadley@utexas.edu>
2020-04-13 13:20:55 -0700

    removed extra activity

--- 21-mern/activities/01-Ins_Mern/create-react-express/client/src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css';
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById(`root`));
-registerServiceWorker();
