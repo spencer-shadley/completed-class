242743f42e8b5ae0522400fcf7392f3422d88e77
Spencer Shadley <spencershadley@utexas.edu>
2020-04-13 13:21:13 -0700

    updated dfs-queen

--- 22-computer-science/3-saturday-graphs/activities/dfs-queen.js
+++ 22-computer-science/3-saturday-graphs/activities/dfs-queen.js
@@ -1,25 +1,27 @@
 // based on https://www.hackerrank.com/challenges/queens-attack-2/problem
 
-let queen = {
+'use strict';
+
+const queenExample = {
     x: 2,
     y: 1
 };
 
-let pieceType = {
-    QUEEN: 'queen',
-    EMPTY: 'empty',
-    BLOCK: 'block'
-}
+const pieceType = {
+    QUEEN: `queen`,
+    EMPTY: `empty`,
+    BLOCK: `block`
+};
 
-let board = [
+const boardExample = [
     [pieceType.EMPTY, pieceType.EMPTY, pieceType.EMPTY, pieceType.EMPTY],
     [pieceType.EMPTY, pieceType.EMPTY, pieceType.BLOCK, pieceType.EMPTY],
     [pieceType.BLOCK, pieceType.QUEEN, pieceType.EMPTY, pieceType.EMPTY],
-    [pieceType.EMPTY, pieceType.EMPTY, pieceType.EMPTY, pieceType.EMPTY],
+    [pieceType.EMPTY, pieceType.EMPTY, pieceType.EMPTY, pieceType.EMPTY]
 ];
 
 function initializeStack(queen) {
-    let stack = [];
+    const stack = [];
     for (let x = -1; x <= 1; ++x) {
         for (let y = -1; y <= 1; ++y) {
             if (x !== 0 || y !== 0) {
@@ -34,24 +36,24 @@ function initializeStack(queen) {
 
     // alternative:
     // return [
-    // {direction: {x: 0, y: 1}, location: {x: queen.x, y: queen.y}}, 
-    // {direction: {x: 0, y: -1}, location: {x: queen.x, y: queen.y}}, 
-    // {direction: {x: 1, y: 0}, location: {x: queen.x, y: queen.y}}, 
-    // {direction: {x: 1, y: 1}, location: {x: queen.x, y: queen.y}}, 
-    // {direction: {x: 1, y: -1}, location: {x: queen.x, y: queen.y}}, 
-    // {direction: {x: -1, y: 0}, location: {x: queen.x, y: queen.y}}, 
-    // {direction: {x: -1, y: -1}, location: {x: queen.x, y: queen.y}}, 
+    // {direction: {x: 0, y: 1}, location: {x: queen.x, y: queen.y}},
+    // {direction: {x: 0, y: -1}, location: {x: queen.x, y: queen.y}},
+    // {direction: {x: 1, y: 0}, location: {x: queen.x, y: queen.y}},
+    // {direction: {x: 1, y: 1}, location: {x: queen.x, y: queen.y}},
+    // {direction: {x: 1, y: -1}, location: {x: queen.x, y: queen.y}},
+    // {direction: {x: -1, y: 0}, location: {x: queen.x, y: queen.y}},
+    // {direction: {x: -1, y: -1}, location: {x: queen.x, y: queen.y}},
     // {direction: {x: -1, y: 1}, location: {x: queen.x, y: queen.y}}];
 }
 
-function withDFS(queen, board) {
-    let stack = initializeStack(queen);
+function DFS(queen, board) {
+    const stack = initializeStack(queen);
 
     let count = 0;
     while (stack.length !== 0) {
-        let move = stack.pop();
-        let x = move.location.x + move.direction.x;
-        let y = move.location.y + move.direction.y;
+        const move = stack.pop();
+        const x = move.location.x + move.direction.x;
+        const y = move.location.y + move.direction.y;
         if (isInBounds(x, y, board.length) && !isBlocked(x, y, board)) {
             ++count;
             stack.push({direction: move.direction, location: {x, y}});
@@ -69,5 +71,5 @@ function isBlocked(x, y, board) {
     return board[x][y] === pieceType.BLOCK;
 }
 
-let count = withDFS(queen, board);
+const count = DFS(queenExample, boardExample);
 console.log(count === 8, count);
