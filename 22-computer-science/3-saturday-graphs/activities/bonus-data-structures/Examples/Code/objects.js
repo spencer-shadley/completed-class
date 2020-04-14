c1ce0586d16b49665f4f97a5a21ca6c58872fc1c
Spencer Shadley <spencershadley@utexas.edu>
2020-04-13 23:56:22 -0700

    added data structures bonus

---
+++ 22-computer-science/3-saturday-graphs/activities/bonus-data-structures/Examples/Code/objects.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+'use strict';
+
+const myPets = {
+    cat: `Mr. Hyena`,
+    lizard: `Mr. Big Big`,
+    goat: `Wolf Who Ate Wall Street`,
+    pigeon: `Joan`
+};
+
+
+const myPetAnimals = [`cat`, `lizard`, `goat`, `pigeon`];
+const myPetNames = [`Mr. Hyena`, `Mr. Big Big`, `Wolf Who Ate Wall Street`, `Joan`];
