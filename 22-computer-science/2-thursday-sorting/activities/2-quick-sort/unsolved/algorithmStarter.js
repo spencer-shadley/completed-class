1929122c6d95e2a9db1bbd49704cad9eafa2b472
Spencer Shadley <spencershadley@utexas.edu>
2020-04-13 23:55:34 -0700

    style fixes to 22.2

--- 22-computer-science/2-thursday-sorting/activities/2-quick-sort/unsolved/algorithmStarter.js
+++ 22-computer-science/2-thursday-sorting/activities/2-quick-sort/unsolved/algorithmStarter.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // ===============================================
 // RUN THIS USING NODE
 // ===============================================
@@ -7,16 +9,16 @@
 // ================================================
 
 // Case 1 - Small Set of Numbers
-var arraySize = 40;
+const arraySize = 40;
 
 // // Case 2 - Large set of Numbers
 // var arraySize = 400000;
 
-var array = [];
-for (var index = 0; index < arraySize; index++) {
-  var randomNumber = Math.round(Math.random() * arraySize);
+const array = [];
+for (let index = 0; index < arraySize; index++) {
+    const randomNumber = Math.round(Math.random() * arraySize);
 
-  array.push(randomNumber);
+    array.push(randomNumber);
 }
 
 // ================================================
@@ -24,15 +26,15 @@ for (var index = 0; index < arraySize; index++) {
 // ================================================
 
 function quickSort(items) {
-  items.sort();
-  return items;
+    items.sort();
+    return items;
 }
 
 // ================================================
 // FUNCTION CALL
 // ================================================
-console.log("PRE-SORT");
-console.log(array.join(" "));
-console.log("---------------------------");
-console.log("POST-SORT");
-console.log(quickSort(array).join(" "));
+console.log(`PRE-SORT`);
+console.log(array.join(` `));
+console.log(`---------------------------`);
+console.log(`POST-SORT`);
+console.log(quickSort(array).join(` `));
