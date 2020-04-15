a4479d81d72045b9309717d44cae2c6a80887c7d
Spencer Shadley <spencershadley@utexas.edu>
2020-04-14 20:36:10 -0700

    removed extra browserrouter

--- class-content/21-MERN/01-Activities/05-Stu_ReactRouter/Solved/client/src/pages/Books.js
+++ class-content/21-MERN/01-Activities/05-Stu_ReactRouter/Solved/client/src/pages/Books.js
@@ -91,13 +91,11 @@ function Books() {
                         <List>
                             {books.map(book =>
                                 <ListItem key={book._id}>
-                                    <BrowserRouter>
-                                        <Link to={`/books/${ book._id}`}>
-                                            <strong>
-                                                {book.title} by {book.author}
-                                            </strong>
-                                        </Link>
-                                    </BrowserRouter>
+                                    <Link to={`/books/${book._id}`}>
+                                        <strong>
+                                            {book.title} by {book.author}
+                                        </strong>
+                                    </Link>
                                     <DeleteBtn onClick={() => deleteBook(book._id)} />
                                 </ListItem>
                             )}
