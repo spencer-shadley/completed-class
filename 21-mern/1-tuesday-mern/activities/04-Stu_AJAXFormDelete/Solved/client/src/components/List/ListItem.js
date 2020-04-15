7f13d3e3c10ec8311a89b8a27222554db0ebaef4
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-04-14 20:02:47 -0700

    Added solved 04 Stu_AJAXFormDelete

---
+++ 21-mern/1-tuesday-mern/activities/04-Stu_AJAXFormDelete/Solved/client/src/components/List/ListItem.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export function ListItem({ children }) {
+    return <li className="list-group-item">{children}</li>;
+}
+ListItem.propTypes = {
+    children: PropTypes.node
+};
