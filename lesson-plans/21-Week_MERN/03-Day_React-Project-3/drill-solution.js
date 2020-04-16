cfd9d9e0657d785d3875eedaef6e68303d8f556a
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-04-16 14:14:40 -0700

    Changed my question solution

--- lesson-plans/21-Week_MERN/03-Day_React-Project-3/drill-solution.js
+++ lesson-plans/21-Week_MERN/03-Day_React-Project-3/drill-solution.js
@@ -1,48 +1,50 @@
 // **************** QUESTION DESCRIPTION **************** //
 
-// In this challenge, we learn how to compute GCD using the Euclidean algorithm.
-
-// The algorithm states that, for computing the GCD of two positive integers x and y, if x and y are equal, GCD(x, y)= x . 
-// Otherwise GCD(x, y) = GCD(x-y,y) if x > y. 
-// There are a few optimizations that can be made to the above logic to arrive at a more efficient implementation.
+// Given an array of numbers
+// Create all unique subsets of the given array
+// [] counts as a subset
 
 // **************** CONSTRAINTS ************************* //
 
-// Validate inputs
+// Only contains numbers
+// Small range
 
 // **************** DEFINE INPUT/OUTPUT/EDGE/OUTLIER **************** //
 
-// Input: Two Integers
-// Output: Integer
+// Input: Array (Of Integers)
+// Output: Array (of Arrays)
 // Edge/Outlier:
 
 // **************** EXAMPLE **************** //
 
-// Input: 1, 5
-// Output: 1
+// Input: [1]
+// Output: [[1], []]
 
-// Input: 10, 100
-// Output: 10
-
-// Input: 22, 131
-// Output: 1
+// Input: [1,2]
+// Output: [[1,2], [1], [2], []]
 
 // **************** PSEUDO CODE SOLUTION / BRAINSTORM **************** //
 
-// Recursively call the function with modulus of x and y
+// Recursively call the function with inclusive and exclusvie options
+// Only add when i reaches length
+// There will be 2 ^ n results, which makes sesne for inclusive/exclusive results
 
 // **************** FUNCTION **************** //
 
-const gcd = (x, y) => {
-
-    if (isNaN(x) || isNaN(y)) {
-        return `Not a valid input`;
+const getAllSubsets = array => {
+    const result = [];
+    const inner = (i, temp) => {
+        // console.log(`At ${i}, this is temp: ${temp}`)
+        if (i === array.length) {
+            result.push(temp);
+            return;
+        }
+        inner(i + 1, temp.concat([array[i]]));
+        inner(i + 1, temp);
     }
-
-    return (y ? gcd(y, x % y) : Math.abs(x));
-
+    inner(0, []);
+    return result;
 }
-
 
 // **************** TESTING **************** //
 
@@ -54,9 +56,14 @@ class Test {
     };
 };
 
-let test1 = new Test('Test 1', 1, gcd(1, 5));
-let test2 = new Test('Test 2', 10, gcd(10, 100));
-let test3 = new Test('Test 3', 1, gcd(22, 131));
+let answer1 = [[1], []];
+let answer2 = [[1, 2], [1], [2], []];
+let answer3 = [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []];
+
+
+let test1 = new Test('Test 1', 2, getAllSubsets([1]).length);
+let test2 = new Test('Test 2', 4, getAllSubsets([1, 2]).length);
+let test3 = new Test('Test 3', 8, getAllSubsets([1, 2, 3]).length);
 
 console.table([
     test1,
