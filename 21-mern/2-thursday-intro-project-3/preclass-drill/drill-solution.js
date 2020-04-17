e947d34f793b9080be701b2e7159b9def69beb5f
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-04-16 18:12:42 -0700

    Added solved PreClassDrill

---
+++ 21-mern/2-thursday-intro-project-3/preclass-drill/drill-solution.js
@@ -0,0 +1,36 @@
+// https://www.hackerrank.com/challenges/functional-programming-warmups-in-recursion---gcd/problem
+
+function GCD(x, y) {
+    return x < y ? GCDhelper(x, y) : GCDhelper(y, x);
+}
+
+function GCDhelper(x, y) {
+    const r = y % x;
+
+    if (r === 0) {
+        return x;
+    }
+
+    return GCDhelper(r, x);
+}
+
+function GCDane(x, y) {
+
+    if (NaN(x) || NaN(y)) {
+        return `Invalid Inputs`
+    };
+
+    return (y ? gcd(y, x % y) : Math.abs(x));
+}
+
+let output = GCD(10, 45);
+console.log(output === 5, output);
+
+output = GCD(1701, 3768);
+console.log(output === 3, output);
+
+output = GCD(1, 1);
+console.log(output === 1, output);
+
+output = GCD(180, 2);
+console.log(output === 2, output);
