b822bc2cc686c3c232618cc32c00359f057f2d57
Spencer Shadley <spencershadley@utexas.edu>
2020-04-19 18:52:15 -0700

    created BST activity

---
+++ 22-computer-science/3-saturday-graphs/activities/2-binary-search-tree/README.md
@@ -0,0 +1,21 @@
+# Binary Search Tree (BST)
+
+## Instructions
+
+- Implement the `search` function to enable finding a value in the BST
+
+## Resources
+
+- use [balanced-binary-search-tree.png](balanced-binary-search-tree.png) as a reference
+
+- https://visualgo.net/en/bst
+
+## Bonus
+
+- Add an `insert` method to `Node` which inserts a new `Node` into the tree while maintaining a BST
+
+  - Don't worry about it being balanced
+
+- Implement the `search` function recursively and iteratively
+
+- After class solve [this problem](https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/)
---
+++ 22-computer-science/3-saturday-graphs/activities/2-binary-search-tree/unsolved/Node.js
@@ -0,0 +1,48 @@
+'use strict';
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = undefined;
+        this.right = undefined;
+    }
+
+    /**
+     * BONUS - implement search first
+     * Inserts a new Node while maintaining a BST
+     * Note: This is very unbalanced and inefficient
+     * Look into Red-Black Tree for a balanced BST solution
+     * @param {Number} value The value of the node being inserted
+     */
+    insert(value) { console.log(value); }
+}
+
+/**
+ * Search @param root for @param value
+ * @param {Node} root the root of the tree to begin the search
+ * @param {Number} value the desired value of the node
+ * @returns {Node} the node with a matching @param value
+ */
+function search(root, value) {
+    console.log(root, value);
+}
+
+function createTreeExample() {
+    const root = new Node(0);
+    root.left = new Node(1);
+    root.right = new Node(2);
+    root.left.left = new Node(3);
+    root.right.left = new Node(5);
+    root.right.right = new Node(6);
+    return root;
+}
+
+const tree = createTreeExample();
+const foundNode = search(tree, 6);
+console.log(foundNode);
+
+// Bonus
+// tree.insert(400);
+// console.log(tree);
+// console.log(tree.right.right.right);
+// console.log(search(tree, 400));
