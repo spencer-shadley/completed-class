33e0f69b09a0a03eaf64288d6486433f74077714
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-04-21 12:56:21 -0700

    Changing soltuion

--- lesson-plans/22-Week_Computer-Science/03-Day_BigO-Data/maxArraySum.js
+++ lesson-plans/22-Week_Computer-Science/03-Day_BigO-Data/maxArraySum.js
@@ -1,42 +1,43 @@
-// **************** QUESTION DESCRIPTION **************** //
+/* 
+**************** QUESTION DESCRIPTION ************************ 
 
-// Given an array of integers, find the subset of non-adjacent elements with the maximum sum.
-// Calculate the sum of that subset.
+    Given an array of integers, find the subset of non-adjacent elements with the maximum sum.
+    Calculate the sum of that subset.
 
-// **************** CONSTRAINTS ************************* //
+***************** CONSTRAINTS ********************************
 
 
 
-// **************** DEFINE INPUT/OUTPUT/EDGE/OUTLIER **************** //
+***************** DEFINE INPUT/OUTPUT/EDGE/OUTLIER ***********
 
-// Input: Array (of integers)
-// Output: Integer (Sum of max subset)
-// Edge/Outlier: Overflow, empty-array
+    Input: Array (of integers)
+    Output: Integer (Sum of max subset)
+    Edge/Outlier: Overflow, empty-array
 
-// **************** EXAMPLE **************** //
+***************** EXAMPLES ************************************
 
-// Input: [-2, 1, 3, -4, 5]
-// Output: 8
-// [3, 5]
-// 3 + 5 = 8
+    Input: [-2, 1, 3, -4, 5]
+    Output: 8
+        [3, 5]
+        3 + 5 = 8
 
-// Input: [3, 7, 4, 6, 5]
-// Output:  13
-// [7, 6]
-// 7 + 6 = 13
+    Input: [3, 7, 4, 6, 5]
+    Output:  13
+        [7, 6]
+        7 + 6 = 13
 
-// Input: [2, 1, 5, 8, 4]
-// Output:  11
-// [2, 5, 4]
-// 2 + 5 + 4 = 11
+    Input: [2, 1, 5, 8, 4]
+    Output:  11
+        [2, 5, 4]
+        2 + 5 + 4 = 11
 
-// **************** PSEUDO CODE SOLUTION / BRAINSTORM **************** //
+***************** PSEUDO CODE SOLUTION / BRAINSTORM ***********
 
-// This is a variation of Kadane's algorithm
-// The only difference is we have an exlucsive/inclusive pattern
-// This is beacuse we can't use adjacent elements
+    This is a variation of Kadane's algorithm
+    The only difference is we have an exlucsive/inclusive pattern
+    This is beacuse we can't use adjacent elements
 
-// **************** FUNCTION **************** //
+**************** FUNCTION ********************************  */
 
 const maxSubsetSum = arr => {
 
@@ -62,7 +63,8 @@ const maxSubsetSum = arr => {
     return (incl > excl) ? incl : excl;
 }
 
-// **************** TESTING **************** //
+
+// **************** TESTING ******************************** //
 class Test {
     constructor(testInput, expectedResult, actualResult) {
         this.testInput = testInput;
