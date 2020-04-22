785b76597d0b1c5cf50ccf71dac78e4126c105a5
Spencer Shadley <spencershadley@utexas.edu>
2020-04-21 17:32:03 -0700

    style fixes

--- class-content/22-computer-science/activities/1-tuesday/3-stu-map/solved.js
+++ class-content/22-computer-science/activities/1-tuesday/3-stu-map/solved.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
+'use strict';
+
 /**
  * O(N) (assuming a HashMap implementation of Map)
- * 
+ *
  * Runtime: 68 ms
- * 
+ *
  * @param {number[]} array
  * @return {number}
  */
@@ -13,20 +16,20 @@ function repeatedNTimes(array) {
         const updatedElementCount = currElementCount ? currElementCount + 1 : 1;
         counts.set(element, updatedElementCount);
     }
-    
+
     for(const [element, count] of counts) {
         if(count === array.length / 2) {
             return element;
         }
     }
     return undefined;
-};
+}
 
 /**
  * O(N^2)
- * 
+ *
  * Runtime: 4340 ms
- * 
+ *
  * @param {number[]} A
  * @return {number}
  */
@@ -44,7 +47,7 @@ function repeatedNTimesSlow(array) {
 
     for (const [element, count] of counts) {
         if (count === array.length / 2) {
-            return element;       
+            return element;
         }
     }
     return undefined;
