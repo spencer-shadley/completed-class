b22043eae6188348cef709ce130cd945d6e92914
Dane Shrewsbury <dshrewsbury2288@gmail.com>
2020-04-21 21:03:56 -0700

    Similar answer to Spencer in unsolved

--- 22-computer-science/1-tuesday-big-o/activities/4-stu-map/unsolved.js
+++ 22-computer-science/1-tuesday-big-o/activities/4-stu-map/unsolved.js
@@ -6,6 +6,30 @@
  * @return {number[]}
  */
 // eslint-disable-next-line no-unused-vars
-function twoSum(nums, target) {
-    console.log(nums, target);
+function twoSum(arr, target) {
+
+    let hash = {};
+
+    for (const number of arr) {
+        hash[number] = target - number;
+    }
+
+    for (let i = 0; i < arr.length; ++i) {
+        if (hash[target - arr[i]]) {
+            return [i, arr.indexOf(hash[arr[i]])]
+        }
+    }
+    return Infinity;
 }
+
+console.log(twoSum([1, 2, 50, 10, 5, 3], 5))
+
+// arr = [1, 2, 50, 10, 5, 3], 5
+// hash = {
+//     1: 4,
+//     2: 3,
+//     50: -45,
+//     10: - 5,
+//     5: 0,
+//     3: 2
+// }
