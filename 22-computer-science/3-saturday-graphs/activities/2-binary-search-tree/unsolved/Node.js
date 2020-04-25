dbb6ade7a87a113905397b5671d15f3059cb2269
Spencer Shadley <spencershadley@utexas.edu>
2020-04-25 10:19:46 -0700

    switched to null

--- 22-computer-science/3-saturday-graphs/activities/2-binary-search-tree/unsolved/Node.js
+++ 22-computer-science/3-saturday-graphs/activities/2-binary-search-tree/unsolved/Node.js
@@ -3,8 +3,8 @@
 class Node {
     constructor(value) {
         this.value = value;
-        this.left = undefined;
-        this.right = undefined;
+        this.left = null;
+        this.right = null;
     }
 
     /**
