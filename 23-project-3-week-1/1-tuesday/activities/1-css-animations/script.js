5ffc36326d18055626b4c29aa3f6f43fce32ce2d
Spencer Shadley <spencershadley@utexas.edu>
2020-04-26 23:17:14 -0700

    added colorful jumping square

--- 23-project-3-week-1/1-tuesday/activities/1-css-animations/script.js
+++ 23-project-3-week-1/1-tuesday/activities/1-css-animations/script.js
@@ -2,9 +2,10 @@
 
 window.addEventListener('load', () => {
     const h1 = document.querySelector('h1');
+    const div = document.querySelector('div');
     const colorButton = document.querySelector('.color-button');
-    
-    colorButton.onclick = () => {
-        h1.classList.toggle('removeColor');
-    }
+    const manyColorsButton = document.querySelector('.many-color-button');
+
+    colorButton.onclick = () => h1.classList.toggle('remove-color');
+    manyColorsButton.onclick = () => div.classList.toggle('many-colors');
 });
